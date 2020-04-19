28fcfc65021a5a67c680ea0c543576aae731bb45
Caleb <calebandcorey@gmail.com>
2020-04-19 13:49:05 +1200

    Created AssessmentStandard class and restored from previous broken project.

---
+++ TraceIt/Models/AssessmentStandard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public class AssessmentStandard
+    {
+        public string AS_Code { get; set; }
+        public string Title { get; set; }
+        public string Sub_Reference { get; set; }
+        public string AS_Type { get; set; }
+        public string Hyperlink { get; set; }
+        public int Credits { get; set; }
+        public int Version { get; set; }
+        public string Domain { get; set; }
+        //public string 
+    }
+}
