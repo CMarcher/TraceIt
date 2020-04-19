54956a636be586a0c5f78912d085bdeec621f10e
Caleb <calebandcorey@gmail.com>
2020-04-19 16:58:24 +1200

    Moved MainPage to the Views folder. Created SubjectSelection page. Changed the startup mainpage.

---
+++ TraceIt/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Views;
+using Xamarin.Forms;
+
+namespace TraceIt
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void buttonConfirm_Clicked(object sender, EventArgs e)
+        {
+            if (InputFieldsInvalid())
+            {
+                await DisplayAlert("There's a problem!", "Please fill the required fields.", "Fine");
+            }
+            else
+            {
+                await Navigation.PushAsync(new SubjectSelectionPage());
+            }
+        }
+
+        public bool InputFieldsInvalid()
+        {
+            if (entryName.Text == "" || pickerLevel.SelectedItem == null)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+    }
+}
