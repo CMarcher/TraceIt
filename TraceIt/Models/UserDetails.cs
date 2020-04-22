3e63b70ce5f3e0c7e2436cb0dc41da83714bb2fe
Caleb <calebandcorey@gmail.com>
2020-04-22 18:02:59 +1200

    Renamed user class appropriately. Implemented fields.

---
+++ TraceIt/Models/UserDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public class UserDetails
+    {
+        public string Name { get; set; }
+        enum Level
+        {
+            Level_One = 1,
+            Level_Two,
+            Level_Three
+        }
+        public int SelectedLevel { get; set; }
+    }
+}
