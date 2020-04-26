48acb1f1287493ef9e1d367d5c266ad2b870431d
Caleb <calebandcorey@gmail.com>
2020-04-26 18:49:42 +1200

    Added SQLite package to solution

--- TraceIt/Models/AssessmentStandardRepository.cs
+++ TraceIt/Models/AssessmentStandardRepository.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace TraceIt.Models
 {
     public class AssessmentStandardRepository
     {
+        public AssessmentStandardRepository()
+        {
 
+        }
     }
 }
