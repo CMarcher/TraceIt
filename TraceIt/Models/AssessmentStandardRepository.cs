c3c7c31189effcd45f4e5559937ecc4eedc8e705
Caleb <calebandcorey@gmail.com>
2020-04-30 13:30:49 +1200

    Initial setup of AS Repo.

--- TraceIt/Models/AssessmentStandardRepository.cs
+++ TraceIt/Models/AssessmentStandardRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using SQLite;
 
@@ -7,9 +8,12 @@ namespace TraceIt.Models
 {
     public class AssessmentStandardRepository
     {
+        SQLiteConnection database;
         public AssessmentStandardRepository()
         {
-
+            string filename = "";
+            string filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), filename);
+            database = new SQLiteConnection(filepath);
         }
     }
 }
