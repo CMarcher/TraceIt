073507c3d06aae269f18284685fda9ef47a89c7a
Caleb <calebandcorey@gmail.com>
2020-05-16 19:57:54 +1200

    Imported implementation of NavigationService (not copyrighted). Configuring pages in App.cs.

---
+++ TraceIt/Utilities/ViewNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Utilities
+{
+    public static class ViewNames
+    {
+        public const string MainPage = "MainPage";
+        public const string SubjectSelectionPage = "SubjectSelectionPage";
+        public const string StandardSelectionPage = "StandardSelectionPage";
+    }
+}
