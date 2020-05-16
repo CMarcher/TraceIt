2a80c6c848fc9a0c2325cb564f598939b7e2b468
Caleb <calebandcorey@gmail.com>
2020-05-16 19:33:41 +1200

    Formed the specifics for INavigationService.

--- TraceIt/Services/INavigationService.cs
+++ TraceIt/Services/INavigationService.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace TraceIt.Services
 {
     public interface INavigationService
     {
+        Page MainPage { get; }
+
+        void Configure(string key, Type pageType);
+        void GoBack();
+        void NavigateTo(string pagekey, object parameter = null);
+
     }
 }
--- TraceIt/ViewModels/StandardSelectionPageViewModel.cs
+++ TraceIt/ViewModels/StandardSelectionPageViewModel.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace TraceIt.ViewModels
 {
-    class StandardSelectionPageViewModel
+    public class StandardSelectionPageViewModel
     {
+
     }
 }
