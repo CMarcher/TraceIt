c5371730e793bb79753fd5728ce951624ba1c3cc
Caleb <calebandcorey@gmail.com>
2020-06-21 14:19:54 +1200

    Adjusted NavigationPage properties for SubjectsSelection and created SubjectsPageViewModel.

---
+++ TraceIt/ViewModels/SubjectsPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TraceIt.ViewModels
+{
+    public class SubjectsPageViewModel
+    {
+        public ObservableCollection<string> Subjects = new ObservableCollection<string>()
+        {
+            "English"
+        };
+    }
+}
