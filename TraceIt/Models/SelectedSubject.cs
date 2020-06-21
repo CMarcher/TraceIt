2130c582fb89c16d622666c44add0625a1ff8509
Caleb <calebandcorey@gmail.com>
2020-06-21 17:03:10 +1200

    Added the framework for selected subjects grid. Created SelectedSubject object.

---
+++ TraceIt/Models/SelectedSubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public class SelectedSubject
+    {
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public int Standards { get; set; }
+
+        public SelectedSubject(string name, int credits, int standards)
+        {
+            Name = name;
+            Credits = credits;
+            Standards = standards;
+        }
+    }
+}
