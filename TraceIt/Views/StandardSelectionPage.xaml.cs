cc8d0201f0758ad0c1f82caa6c4f0a77753f70a9
Caleb <calebandcorey@gmail.com>
2020-07-06 16:33:24 +1200

    Created corresponding local ViewModel and set CollectionView ItemsSource to Standards.

--- TraceIt/Views/StandardSelectionPage.xaml.cs
+++ TraceIt/Views/StandardSelectionPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,14 @@ namespace TraceIt.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StandardSelectionPage : ContentPage
     {
+        public StandardSelectionPageViewModel ViewModel = new StandardSelectionPageViewModel();
+
         public StandardSelectionPage()
         {
             InitializeComponent();
+
+            BindingContext = ViewModel;
+            selectedStandardsCollectionView.ItemsSource = ViewModel.Standards;
         }
     }
 }
