26d91a124913e8c340430be67203ecb49e391b7e
Caleb <calebandcorey@gmail.com>
2020-07-06 16:32:08 +1200

    Created an ObservableCollection of standards for the selected subject, inserted some items.

--- TraceIt/ViewModels/StandardSelectionPageViewModel.cs
+++ TraceIt/ViewModels/StandardSelectionPageViewModel.cs
@@ -1,11 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using TraceIt.Models;
 
 namespace TraceIt.ViewModels
 {
     public class StandardSelectionPageViewModel
     {
+        public ObservableCollection<AssessmentStandard> Standards = new ObservableCollection<AssessmentStandard>()
+        {
+            new AssessmentStandard()
+            {
+                AS_Code = 97321,
+                Title = "Demonstrate understanding of something",
+                Sub_Reference = "Smart English 1.1",
+                AS_Type = "Achievement",
+                Hyperlink = "Whatever",
+                Credits = 4
+            },
+            new AssessmentStandard()
+            {
+                AS_Code = 97321,
+                Title = "Demonstrate understanding of something",
+                Sub_Reference = "Smart English 1.1",
+                AS_Type = "Achievement",
+                Hyperlink = "Whatever",
+                Credits = 4
+            },
+            new AssessmentStandard()
+            {
+                AS_Code = 97321,
+                Title = "Demonstrate understanding of something",
+                Sub_Reference = "Smart English 1.1",
+                AS_Type = "Achievement",
+                Hyperlink = "Whatever",
+                Credits = 4
+            },
+            new AssessmentStandard()
+            {
+                AS_Code = 97321,
+                Title = "Demonstrate understanding of something",
+                Sub_Reference = "Smart English 1.1",
+                AS_Type = "Achievement",
+                Hyperlink = "Whatever",
+                Credits = 4
+            },
+            new AssessmentStandard()
+            {
+                AS_Code = 97321,
+                Title = "Demonstrate understanding of something",
+                Sub_Reference = "Smart English 1.1",
+                AS_Type = "Achievement",
+                Hyperlink = "Whatever",
+                Credits = 4
+            }
+
+        };
 
     }
 }
