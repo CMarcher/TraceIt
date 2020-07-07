6b99c3ef0d61d8b021016365f6f66b025817842a
Caleb <calebandcorey@gmail.com>
2020-07-07 12:57:51 +1200

    Created BoolConverter.

---
+++ TraceIt/BoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt
+{
+    public class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isVisible = (bool)value;
+
+            return !isVisible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
