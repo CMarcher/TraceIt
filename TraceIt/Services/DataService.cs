bec07db5614c7c89b6fed1d041b592f3a0884a36
CMarcher <calebandcorey@gmail.com>
2020-07-09 13:29:20 +1200

    Implemented DataService functionality to load database file without having to deal with Dependency Injection.

--- TraceIt/Services/DataService.cs
+++ TraceIt/Services/DataService.cs
@@ -1,10 +1,55 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Extensions;
+using TraceIt.Utilities;
+using Xamarin.Essentials;
 
 namespace TraceIt.Services
 {
-    public class DataService
+    public class DataService : IDataService
     {
+        public static SQLiteAsyncConnection Database;
+        public bool Initialised = false;
+
+        public static readonly Lazy<Task<SQLiteAsyncConnection>> CreateLazyConnection = new Lazy<Task<SQLiteAsyncConnection>>(async () =>
+        {
+            var databaseName = FileNames.DatabaseName;
+            string documentsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var databasePath = Path.Combine(documentsDirectory, databaseName);
+
+            bool fileExists = File.Exists(databasePath);
+            if (!fileExists)
+            {
+                using (var assetStream = await FileSystem.OpenAppPackageFileAsync(databaseName))
+                using (var fileStream = new FileStream(databasePath, FileMode.Create, FileAccess.Write))
+                {
+                    await assetStream.CopyToAsync(fileStream);
+                }
+            }
+
+            var connection = new SQLiteAsyncConnection(databasePath, Constants.Flags);
+            return connection;
+        });
+
+        public DataService()
+        {
+            InitialiseAsync().SafeFireAndForget(false);
+        }
+
+        public async Task InitialiseAsync()
+        {
+            if (!Initialised)
+            {
+                Database = await CreateLazyConnection.Value;
+                Initialised = true;
+            }
+        }
+
+
     }
 }
