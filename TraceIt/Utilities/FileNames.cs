ee1d10af1db41db4a96bf59a84276988f7bc8548
CMarcher <calebandcorey@gmail.com>
2020-07-09 13:31:35 +1200

    Created FileNames class to return file names consistently, avoiding errors caused by trivial mistakes.

---
+++ TraceIt/Utilities/FileNames.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Utilities
+{
+    public static class FileNames
+    {
+        public const string DatabaseName = "AssessmentStandards.db3";
+    }
+}
