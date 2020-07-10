178fda14ef1f7ff9fa7985c7428b67c56e4c33fc
CMarcher <calebandcorey@gmail.com>
2020-07-10 14:48:51 +1200

    Created custom NavigationPage with a gradient background.

---
+++ TraceIt/Controls/GradientNavigationPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Controls
+{
+    public class GradientNavigationPage : NavigationPage
+    {
+        public GradientNavigationPage()
+        {
+            BarTextColor = Color.White;
+        }
+    }
+}
