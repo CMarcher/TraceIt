8f7694f99ab0cdd74249380c9411e2b746925cd3
CMarcher <calebandcorey@gmail.com>
2020-07-18 21:58:27 +1200

    Added GradientShellPageRenderer to replace GradientTabbedPageRenderer, implemented gradients for navigation bar and bottom navigation.

---
+++ TraceIt.Android/Renderers/GradientShellPageRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using TraceIt.Controls;
+using TraceIt.Droid.Renderers;
+using TraceIt.Droid.Renderers.Appearance_Trackers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(GradientShellPage), typeof(GradientShellPageRenderer))]
+namespace TraceIt.Droid.Renderers
+{
+    public class GradientShellPageRenderer : ShellRenderer
+    {
+        public GradientShellPageRenderer(Context context) : base(context) { }
+
+        protected override IShellToolbarAppearanceTracker CreateToolbarAppearanceTracker()
+        {
+            return new GradientShellToolbarAppearanceTracker(this);
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new GradientShellBottomTabBarAppearanceTracker(this, shellItem);
+        }
+
+        protected override IShellSectionRenderer CreateShellSectionRenderer(ShellSection shellSection)
+        {
+            return base.CreateShellSectionRenderer(shellSection);
+        }
+
+        public GradientShellPage GetPageInstance()
+        {
+            return (GradientShellPage)Element;
+        }
+    }
+}
