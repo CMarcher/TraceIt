43d516dff818526344ea7856da361a408fbeee27
CMarcher <calebandcorey@gmail.com>
2020-07-18 12:56:40 +1200

    Created bindable gradient properties for gradient colours.

--- TraceIt/Controls/GradientTabbedPage.cs
+++ TraceIt/Controls/GradientTabbedPage.cs
@@ -11,5 +11,29 @@ namespace TraceIt.Controls
         {
             BarTextColor = Color.White;
         }
+
+        public static readonly BindableProperty BottomColorProperty =
+        BindableProperty.Create(propertyName: nameof(BottomColor),
+           returnType: typeof(Color),
+           declaringType: typeof(GradientTabbedPage),
+           defaultValue: Color.FromHex("#0033D5")); 
+
+        public static readonly BindableProperty TopColorProperty =
+            BindableProperty.Create(propertyName: nameof(TopColor),
+                returnType: typeof(Color),
+                declaringType: typeof(GradientTabbedPage),
+                defaultValue: Color.FromHex("#002394"));
+
+        public Color BottomColor
+        {
+            get { return (Color)GetValue(BottomColorProperty); }
+            set { SetValue(BottomColorProperty, value); }
+        }
+
+        public Color TopColor
+        {
+            get { return (Color)GetValue(TopColorProperty); }
+            set { SetValue(TopColorProperty, value); }
+        }
     }
 }
