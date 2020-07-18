79a7b8ae78b1d38186fbe5720cc40fbbd6383657
CMarcher <calebandcorey@gmail.com>
2020-07-18 13:14:09 +1200

    Added gradient renderers for navigation and tabbed page.

---
+++ TraceIt.iOS/Renderers/GradientTabbedPageRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace TraceIt.iOS.Renderers
+{
+    public class GradientTabbedPageRenderer : TabbedRenderer
+    {
+    }
+}
