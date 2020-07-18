b99b4d1e5602f471b3342ff419ecf23b191633eb
CMarcher <calebandcorey@gmail.com>
2020-07-18 13:08:23 +1200

    Changed TabbedPage to custom GradientTabbedPage.

--- TraceIt/Views/TabbedPageHome.xaml.cs
+++ TraceIt/Views/TabbedPageHome.xaml.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TraceIt.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TabbedPageHome : TabbedPage
+    public partial class TabbedPageHome : GradientTabbedPage
     {
         public TabbedPageHome()
         {
