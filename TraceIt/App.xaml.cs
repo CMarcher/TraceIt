340961daaed110f9b4e3cf6120368445e66f944b
CMarcher <calebandcorey@gmail.com>
2020-07-18 21:56:02 +1200

    Removed NavigationService configuration.

--- TraceIt/App.xaml.cs
+++ TraceIt/App.xaml.cs
@@ -17,10 +17,6 @@ namespace TraceIt
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjg0ODA1QDMxMzgyZTMyMmUzMFlYWEFCOEZOODM4dXB2QmpHMmlVM3VuZi9kdzhXUFB4SlNDVHhwVmtMWkE9");
             InitializeComponent();
 
-            NavigationService.Configure(ViewNames.SubjectSelectionPage, typeof(SubjectSelectionPage));
-            NavigationService.Configure(ViewNames.SubjectsPage, typeof(SubjectsPage));
-            NavigationService.Configure(ViewNames.TabbedPageHome, typeof(TabbedPageHome));
-
             MainPage = new MainPage();
         }
 
