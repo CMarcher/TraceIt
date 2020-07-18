c721773f28d859213ca79ac72ce053acd59168fe
CMarcher <calebandcorey@gmail.com>
2020-07-18 21:54:13 +1200

    Added ShellHomePage and added Tabs and ShellContent elements.

---
+++ TraceIt/Views/ShellHomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Controls;
+using TraceIt.Views.Charts;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TraceIt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShellHomePage : GradientShellPage
+    {
+        public ShellHomePage()
+        {
+            InitializeComponent();
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute("subjectsPage", typeof(SubjectsPage));
+            Routing.RegisterRoute("creditsChartPage", typeof(CreditsChartPage));
+            Routing.RegisterRoute("endorsementsChartPage", typeof(CreditsChartPage));
+        }
+    }
+}
