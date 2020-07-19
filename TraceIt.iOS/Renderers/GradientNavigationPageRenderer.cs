2a1689c2bf7200dc2e76b4fb2367c66e6bb2edc3
CMarcher <calebandcorey@gmail.com>
2020-07-19 18:37:37 +1200

    Implemented GradientNavigationPageRenderer gradient layer.

--- TraceIt.iOS/Renderers/GradientNavigationPageRenderer.cs
+++ TraceIt.iOS/Renderers/GradientNavigationPageRenderer.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CoreAnimation;
 using Foundation;
+using TraceIt.Controls;
+using TraceIt.iOS.Renderers;
 using UIKit;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+[assembly: ExportRenderer(typeof(GradientNavigationPage), typeof(GradientNavigationPageRenderer))]
 namespace TraceIt.iOS.Renderers
 {
     public class GradientNavigationPageRenderer : NavigationRenderer
     {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var control = (GradientNavigationPage)Element;
+
+            var gradient = new CAGradientLayer()
+            {
+                Colors = new[]
+                {
+                    control.TopColor.ToCGColor(),
+                    control.BottomColor.ToCGColor()
+                },
+
+                Bounds = NavigationBar.Bounds,
+                Locations = new NSNumber[] { 0, 1 }
+            };
+
+            NavigationBar.Layer.AddSublayer(gradient);
+        }
     }
 }
