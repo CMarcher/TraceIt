48c54b3f9a37cc8f3b3686ef89f3f9567a12f3ea
CMarcher <calebandcorey@gmail.com>
2020-07-19 18:35:52 +1200

    Created CustomEntryRenderer and implemented the changes.

---
+++ TraceIt.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using TraceIt.Controls;
+using TraceIt.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace TraceIt.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
