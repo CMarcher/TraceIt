4ed64636a0085a8608d141fcf436a77a91337510
CMarcher <calebandcorey@gmail.com>
2020-07-19 18:36:25 +1200

    Created CustomPickerRenderer and implemented the changes.

---
+++ TraceIt.iOS/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using TraceIt.Controls;
+using TraceIt.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+namespace TraceIt.iOS.Renderers
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
