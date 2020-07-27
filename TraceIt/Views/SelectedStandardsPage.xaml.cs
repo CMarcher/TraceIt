a25410efde68080c94c4b0fcf9139239a46aface
CMarcher <calebandcorey@gmail.com>
2020-07-27 20:26:10 +1200

    Added buttonAdd click event.

--- TraceIt/Views/SelectedStandardsPage.xaml.cs
+++ TraceIt/Views/SelectedStandardsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TraceIt.Controls;
 using TraceIt.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -25,6 +26,11 @@ namespace TraceIt.Views
         private void buttonViewMore_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new StandardDetailPage());
+        }
+
+        private void buttonAdd_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new GradientNavigationPage(new StandardCategorisedPage()));
         }
     }
 }
