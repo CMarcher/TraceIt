fee076bf356ebb1221cbcfdad24f4225ee74731c
CMarcher <calebandcorey@gmail.com>
2020-07-28 20:36:23 +1200

    Added SetSubjectsAsync to get the subjects from the database, method used in constructor.

--- TraceIt/ViewModels/SubjectSelectionPageViewModel.cs
+++ TraceIt/ViewModels/SubjectSelectionPageViewModel.cs
@@ -3,44 +3,24 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Dynamic;
 using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Extensions;
 using TraceIt.Models;
 
 namespace TraceIt.ViewModels
 {
     public class SubjectSelectionPageViewModel
     {
-        public ObservableCollection<Subject> subjects = new ObservableCollection<Subject>()
-        {
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" },
-                new Subject() { Name = "Subject" }
-
-        };
+        public ObservableCollection<Subjects> subjects = new ObservableCollection<Subjects>();
 
         public SubjectSelectionPageViewModel()
         {
-            
+            SetSubjectsAsync().SafeFireAndForget(false);
+        }
+
+        async Task SetSubjectsAsync()
+        {
+            subjects = await App.DataService.GetSubjectsAsync();
         }
     }
 }
