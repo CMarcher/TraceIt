c696cf13ff89c8e43641ecf47142fc9d46180392
CMarcher <calebandcorey@gmail.com>
2020-07-29 21:02:06 +1200

    Created AppearanceTracker for TopTabsBar.

---
+++ TraceIt.Android/Renderers/Appearance Trackers/GradientShellTopTabBarAppearanceTracker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gestures;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace TraceIt.Droid.Renderers.Appearance_Trackers
+{
+    public class GradientShellTopTabBarAppearanceTracker : ShellTabLayoutAppearanceTracker
+    {
+        IShellContext context;
+
+        public GradientShellTopTabBarAppearanceTracker(IShellContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public override void SetAppearance(TabLayout tabLayout, ShellAppearance appearance)
+        {
+            base.SetAppearance(tabLayout, appearance);
+
+            var renderer = (GradientShellPageRenderer)context;
+            var page = renderer.GetPageInstance();
+
+            var gradient = new GradientDrawable(
+                GradientDrawable.Orientation.LeftRight,
+                new int[] {
+                    page.ToolbarTopColor.ToAndroid(),
+                    page.ToolbarBottomColor.ToAndroid() }
+                );
+
+            tabLayout.SetBackground(gradient);
+        }
+    }
+}
