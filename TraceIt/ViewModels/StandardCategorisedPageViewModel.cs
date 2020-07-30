f9c9ca7abea47426413ef6a35439ad6351efec6d
Caleb Archer <calebandcorey@gmail.com>
2020-07-30 12:16:37 +1200

    Created StandardCategorisedPageViewModel and implemented partial command.

---
+++ TraceIt/ViewModels/StandardCategorisedPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Extensions;
+using TraceIt.Models;
+using Xamarin.Forms;
+
+namespace TraceIt.ViewModels
+{
+    public class StandardCategorisedPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Subject> Subjects { get; set; }
+
+        public StandardCategorisedPageViewModel()
+        {
+            SetSubjects().SafeFireAndForget(false);
+        }
+
+        async Task SetSubjects() => Subjects = await App.DataService.GetSubjectsAsync();
+    }
+}
