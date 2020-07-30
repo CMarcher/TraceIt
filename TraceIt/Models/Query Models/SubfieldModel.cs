ea7effdcdc02ab688d9209220be7f37f49d41836
Caleb Archer <calebandcorey@gmail.com>
2020-07-31 10:26:49 +1200

    Created SubfieldModel with Subfield property.

---
+++ TraceIt/Models/Query Models/SubfieldModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models.Query_Models
+{
+    public class SubfieldModel
+    {
+        [NotNull]
+        public string Subfield { get; set; }
+    }
+}
