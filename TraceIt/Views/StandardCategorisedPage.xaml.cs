0c27d66a4c3ad1b56e99022e16c03b4afe00ac44
Caleb Archer <calebandcorey@gmail.com>
2020-07-30 12:17:27 +1200

    Added ViewModel setting to Page.

--- TraceIt/Views/StandardCategorisedPage.xaml.cs
+++ TraceIt/Views/StandardCategorisedPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,19 @@ namespace TraceIt.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StandardCategorisedPage : ContentPage
     {
+        StandardCategorisedPageViewModel ViewModel;
+
         public StandardCategorisedPage()
         {
             InitializeComponent();
+
+            ViewModel = new StandardCategorisedPageViewModel();
+            SetItemsSource();
+        }
+
+        void SetItemsSource()
+        {
+            collectionViewCategories.ItemsSource = ViewModel.Subjects;
         }
     }
 }
