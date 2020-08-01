7c148fd6e2030909148e7d3aad6ffbf1f90c8599
CMarcher <calebandcorey@gmail.com>
2020-08-01 12:42:11 +1200

    Added SetSubfields method whicb returns the Subfields found in the Database. Called method with SafeFireAndForget extension.

--- TraceIt/ViewModels/StandardCategorisedPageViewModel.cs
+++ TraceIt/ViewModels/StandardCategorisedPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Extensions;
 using TraceIt.Models;
+using TraceIt.Models.Query_Models;
 using Xamarin.Forms;
 
 namespace TraceIt.ViewModels
@@ -12,12 +13,17 @@ namespace TraceIt.ViewModels
     public class StandardCategorisedPageViewModel : BaseViewModel
     {
         public ObservableCollection<Subject> Subjects { get; set; }
+        public List<SubfieldModel> Subfields { get; set; }
 
         public StandardCategorisedPageViewModel()
         {
             SetSubjects().SafeFireAndForget(false);
+            SetSubfields().SafeFireAndForget(false);
         }
 
         async Task SetSubjects() => Subjects = await App.DataService.GetSubjectsAsync();
+
+        async Task SetSubfields() => Subfields = await App.DataService.GetSubfieldsAsync(Services.DataService.FilterOptions.Unit);
+
     }
 }
