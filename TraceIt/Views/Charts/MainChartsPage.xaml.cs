de5197ede7e01705d8c4ea2f154d27134445cfb0
Caleb Archer <calebandcorey@gmail.com>
2020-08-03 15:15:19 +1200

    Created MainChartsPage to contain sub-charts.

---
+++ TraceIt/Views/Charts/MainChartsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TraceIt.Views.Charts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainChartsPage : ContentPage
+    {
+        public MainChartsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
