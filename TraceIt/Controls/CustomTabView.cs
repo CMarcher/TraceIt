4fda5add520364504de56db9c373a0be5aaabb51
CMarcher <calebandcorey@gmail.com>
2020-08-03 21:54:30 +1200

    Created CustomTabView control.

---
+++ TraceIt/Controls/CustomTabView.cs
@@ -0,0 +1,11 @@
+﻿using Syncfusion.XForms.TabView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Controls
+{
+    public class CustomTabView : SfTabView
+    {
+    }
+}
