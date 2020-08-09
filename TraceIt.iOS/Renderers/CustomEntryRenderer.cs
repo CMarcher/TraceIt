cfe25efc836337fda11f125f9cd920f2426a2575
CMarcher <calebandcorey@gmail.com>
2020-08-10 06:56:10 +1200

    Added control styling for corner radius and padding.

--- TraceIt.iOS/Renderers/CustomEntryRenderer.cs
+++ TraceIt.iOS/Renderers/CustomEntryRenderer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CoreGraphics;
 using Foundation;
 using TraceIt.Controls;
 using TraceIt.iOS.Renderers;
@@ -22,7 +22,14 @@ namespace TraceIt.iOS.Renderers
             if (Element != null)
             {
                 Control.BorderStyle = UITextBorderStyle.None;
-                Control.VerticalAlignment = UIControlContentVerticalAlignment.Fill;
+                Control.Layer.CornerRadius = 50;
+
+                Control.LeftView = new UIView(new CGRect(0, 0, 20, Control.Frame.Height));
+                Control.RightView = new UIView(new CGRect(0, 0, 20, Control.Frame.Height));
+                Control.LeftViewMode = UITextFieldViewMode.Always;
+                Control.RightViewMode = UITextFieldViewMode.Always;
+
+                Element.HeightRequest = 40;
             }
                 
         }
