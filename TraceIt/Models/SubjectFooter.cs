46c2fbda7413b4331795c6e73fe4b4a4a2853773
CMarcher <calebandcorey@gmail.com>
2020-08-09 18:36:47 +1200

    Created SubjectFooter and set to inherit from ISubjectItem

---
+++ TraceIt/Models/SubjectFooter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public class SubjectFooter : ISubjectItem
+    {
+        public SubjectFooter()
+        {
+
+        }
+    }
+}
