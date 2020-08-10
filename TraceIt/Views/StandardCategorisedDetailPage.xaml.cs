9af4b2dfbf31cb4e54594cf99e7e5c1878b4953c
CMarcher <calebandcorey@gmail.com>
2020-08-10 21:33:15 +1200

    Created StandardCategorisedDetailPage, added SfListView ItemsSource set to constructor parameter.

---
+++ TraceIt/Views/StandardCategorisedDetailPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Extensions;
+using TraceIt.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TraceIt.Services;
+
+namespace TraceIt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandardCategorisedDetailPage : ContentPage
+    {
+        public StandardCategorisedDetailPage(string parameter, DataService.FilterByOption filterByOption)
+        {
+            InitializeComponent();
+            Task.Run(() => SetStandards(parameter, filterByOption)).Wait();
+        }
+
+        async Task SetStandards(string parameter, DataService.FilterByOption filterByOption)
+        {
+            categorisedStandardsList.ItemsSource = await App.DataService.GetFilteredStandardsAsync(
+                parameter, filterByOption);
+        }
+    }
+}
