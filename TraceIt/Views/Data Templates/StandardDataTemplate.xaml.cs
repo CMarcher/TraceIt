9fc39f88b30481e0a20783fb4e73f2cbbfbd6ce6
Caleb Archer <calebandcorey@gmail.com>
2020-08-14 10:45:07 +1200

    Created StandardDataTemplate.

---
+++ TraceIt/Views/Data Templates/StandardDataTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TraceIt.Views.Data_Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandardDataTemplate : ViewCell
+    {
+        public StandardDataTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
