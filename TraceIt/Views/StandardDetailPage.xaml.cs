a2b18308d8abbc49622fa710f983cb0194492f40
CMarcher <calebandcorey@gmail.com>
2020-08-14 21:18:45 +1200

    Added Models reference. Added AssessmentStandard parameter to constructor.

--- TraceIt/Views/StandardDetailPage.xaml.cs
+++ TraceIt/Views/StandardDetailPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +12,18 @@ namespace TraceIt.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StandardDetailPage : ContentPage
     {
-        public StandardDetailPage()
+        AssessmentStandard Standard { get; set; }
+
+        public StandardDetailPage(AssessmentStandard standard = null)
         {
             InitializeComponent();
+            BindingContext = standard;
+            Standard = standard;
         }
 
         private void buttonAccess_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new StandardWebPage());
+            Navigation.PushAsync(new StandardWebPage(Standard));
         }
     }
 }
