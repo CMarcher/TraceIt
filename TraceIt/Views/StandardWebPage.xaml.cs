aa72a3fe70db0398b4f56b4d800eaf57ffb08e59
CMarcher <calebandcorey@gmail.com>
2020-08-14 21:22:19 +1200

    Set BindingContext to the AssessmentStandard parameter.

--- TraceIt/Views/StandardWebPage.xaml.cs
+++ TraceIt/Views/StandardWebPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,10 @@ namespace TraceIt.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StandardWebPage : ContentPage
     {
-        public StandardWebPage()
+        public StandardWebPage(AssessmentStandard standard)
         {
             InitializeComponent();
+            webView.BindingContext = standard;
         }
     }
 }
