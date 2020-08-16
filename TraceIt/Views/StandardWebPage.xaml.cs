0ba80b53ac8fd69779f57702ef228b3c65639c5e
CMarcher <calebandcorey@gmail.com>
2020-08-16 18:41:08 +1200

    Added ToolbarItem that prompts the user to confirm whether to go to NZQA's site or not.

--- TraceIt/Views/StandardWebPage.xaml.cs
+++ TraceIt/Views/StandardWebPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Models;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +17,19 @@ namespace TraceIt.Views
         {
             InitializeComponent();
             webView.BindingContext = standard;
+            if (Device.RuntimePlatform == Device.iOS)
+                webView.Source = standard.Hyperlink;
+            else if (Device.RuntimePlatform == Device.Android)
+                webView.Source = "https://docs.google.com/viewer?url=" + standard.Hyperlink;
+        }
+
+        private async void launchWebToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            bool launchWeb = await DisplayAlert("Leaving TraceIt", "This will take you to NZQA's website, outside the app. Continue?",
+                "Sure!", "Never mind");
+
+            if (launchWeb)
+                await Launcher.OpenAsync("https://www.nzqa.govt.nz/ncea/subjects/");
         }
     }
 }
