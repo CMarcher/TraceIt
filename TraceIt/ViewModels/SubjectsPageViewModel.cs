9664902a052fda2524871d457f298a43d1d20ee7
CMarcher <calebandcorey@gmail.com>
2020-08-16 21:49:42 +1200

    Set Subjects to have a public getter and private setter. Created SetSubjects method and called in constructor.

--- TraceIt/ViewModels/SubjectsPageViewModel.cs
+++ TraceIt/ViewModels/SubjectsPageViewModel.cs
@@ -4,20 +4,24 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 using TraceIt.Models;
 
 namespace TraceIt.ViewModels
 {
     public class SubjectsPageViewModel : BaseViewModel
     {
-        public ObservableCollection<Subject> Subjects = new ObservableCollection<Subject>()
+        public ObservableCollection<Subject> Subjects { get; private set; }
+
+        public SubjectsPageViewModel()
         {
-            new Subject() { Name = "Chemistry" },
-            new Subject() { Name = "Sound Production" },
-            new Subject() { Name = "English" },
-            new Subject() { Name = "Computer Studies" },
-            new Subject() { Name = "Calculus" }
-        };
+            Task.Run(SetSubjects).Wait();
+        }
+
+        private async Task SetSubjects()
+        {
+            Subjects = await App.DataService.GetSelectedSubjectsAsync();
+        }
 
         
     }
