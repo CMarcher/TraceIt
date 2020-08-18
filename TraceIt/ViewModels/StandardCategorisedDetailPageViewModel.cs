7a2b7ab042013c62378cdf1b91c7d0b3121a3ec2
CMarcher <calebandcorey@gmail.com>
2020-08-18 20:16:11 +1200

    Created StandardCategorisedDetailPageViewModel and implemented some methods and the constructor.

---
+++ TraceIt/ViewModels/StandardCategorisedDetailPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Models;
+using TraceIt.Services;
+using TraceIt.Views;
+using Xamarin.Forms;
+
+namespace TraceIt.ViewModels
+{
+    public class StandardCategorisedDetailPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<AssessmentStandard> Standards { get; private set; }
+        public Command NavigateToDetailCommand { get; private set; }
+
+        public StandardCategorisedDetailPageViewModel(string parameter, DataService.FilterByOption filterByOption)
+        {
+            Task.Run(() => SetStandards(parameter, filterByOption)).Wait();
+
+            NavigateToDetailCommand = new Command<AssessmentStandard>(async (standard) => await NavigateToDetail(standard));
+        }
+
+        async Task SetStandards(string parameter, DataService.FilterByOption filterByOption) =>
+            Standards = await App.DataService.GetCategorisedStandardsAsync(
+                              parameter, filterByOption);
+        
+        async Task NavigateToDetail(AssessmentStandard standard) => 
+            await App.Current.MainPage.Navigation.PushAsync(new StandardDetailPage(standard));
+    }
+}
