452b08be26ac96d237d81fcf9cd1d2ae43893153
CMarcher <calebandcorey@gmail.com>
2020-08-24 21:55:59 +1200

    Created selection and deselection data triggers that respond to when a standard's being selected changes.

---
+++ TraceIt/Triggers/DeselectedTriggerAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Triggers
+{
+    public class DeselectedTriggerAction : TriggerAction<VisualElement>
+    {
+        protected override void Invoke(VisualElement sender)
+        {
+            var button  = sender as Button;
+
+            button.BackgroundColor = Color.Yellow;
+            button.TextColor = Color.Black;
+            button.Text = "Add";
+        }
+    }
+}
---
+++ TraceIt/Triggers/SelectedTriggerAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Triggers
+{
+    public class SelectedTriggerAction : TriggerAction<VisualElement>
+    {
+        protected override void Invoke(VisualElement sender)
+        {
+            var button = sender as Button;
+
+            button.BackgroundColor = Color.Red;
+            button.Text = "Remove";
+            button.TextColor = Color.White;
+        }
+    }
+}
