78133e495c34da6d0b4560cf6f87fa1fbede1089
CMarcher <calebandcorey@gmail.com>
2020-08-24 21:54:22 +1200

    Created BaseModel that implements INotifyPropertyChanged.

---
+++ TraceIt/Models/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public abstract class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
