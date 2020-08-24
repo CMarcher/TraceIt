10e91729d20b0426c2ba6c5f4b426e9ea9063296
CMarcher <calebandcorey@gmail.com>
2020-08-24 21:55:04 +1200

    Class inherits from BaseModel, set the getters and setters accordingly to property change structure.

--- TraceIt/Models/Subject.cs
+++ TraceIt/Models/Subject.cs
@@ -8,22 +8,58 @@ using System.Text;
 namespace TraceIt.Models
 {
     [Table("Subjects")]
-    public class Subject : ISubjectItem
+    public class Subject : BaseModel, ISubjectItem 
     {
+        private int _id;
         [PrimaryKey]
-        public int ID { get; set; }
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
 
+        private string _name;
         [NotNull]
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
 
         [NotNull]
         public string Subfield { get; set; }
 
+        private bool _selected;
         [NotNull]
-        public bool Selected { get; set; } = false;
+        public bool Selected
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                OnPropertyChanged(nameof(Selected));
+            }
+        }
 
+        private bool _custom;
         [NotNull]
-        public bool Custom { get; set; } = false;
+        public bool Custom
+        {
+            get { return _custom; }
+            set
+            {
+                _custom = value;
+                OnPropertyChanged(nameof(_custom));
+            }
+        }
 
         public Subject() { }
     }
