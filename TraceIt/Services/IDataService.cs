a2f6fef114d5319f1a7c314d1cbc98b00789e408
CMarcher <calebandcorey@gmail.com>
2020-08-25 21:55:35 +1200

    Extracted interface from DataService class.

--- TraceIt/Services/IDataService.cs
+++ TraceIt/Services/IDataService.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using TraceIt.Models;
+using TraceIt.Models.Query_Models;
 
 namespace TraceIt.Services
 {
     public interface IDataService
     {
+        Task<ObservableCollection<AssessmentStandard>> GetCategorisedStandardsAsync(string parameter, DataService.FilterByOption filterByOption);
+        Task<List<AssessmentStandard>> GetMatchingStandards(string searchQuery);
+        Task<ObservableCollection<Subject>> GetSelectedSubjectsAsync();
+        Task<AssessmentStandard> GetStandardByIDAsync(int id);
+        Task<ObservableCollection<AssessmentStandard>> GetStandardsAsync();
+        Task<List<SubfieldModel>> GetSubfieldsAsync(DataService.StandardType filterOptions);
+        Task<ObservableCollection<Subject>> GetSubjectsAsync();
+        Task UpdateStandardAsync(AssessmentStandard standard);
+        Task UpdateSubjectAsync(Subject subject);
+        Task UpdateSubjectsAsync(List<Subject> subjects);
     }
 }
