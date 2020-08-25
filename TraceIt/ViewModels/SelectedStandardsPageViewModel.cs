bee762e3c1c3c8505371633ac762f7d4f1b28f38
Caleb Archer <calebandcorey@gmail.com>
2020-08-26 09:42:47 +1200

    Subscribed in constructor to message sent by StandardCategorisedDetailPageViewModel to resfresh Standards.

--- TraceIt/ViewModels/SelectedStandardsPageViewModel.cs
+++ TraceIt/ViewModels/SelectedStandardsPageViewModel.cs
@@ -5,16 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Models;
 using TraceIt.Utilities;
+using Xamarin.Forms;
 
 namespace TraceIt.ViewModels
 {
     public class SelectedStandardsPageViewModel : BaseViewModel
     {
-        public List<AssessmentStandard> Standards = new List<AssessmentStandard>();
+        public ObservableCollection<AssessmentStandard> Standards = new ObservableCollection<AssessmentStandard>();
 
         public SelectedStandardsPageViewModel()
         {
             Task.Run(SetStandards).Wait();
+
+            MessagingCenter.Subscribe<StandardCategorisedDetailPageViewModel>(this, "Update standards", (sender) =>
+            {
+                Task.Run(SetStandards);
+            });
         }
 
         async Task SetStandards() => Standards = await App.DataService.GetSelectedStandards(StatusTracker.CurrentSubject.Name);
