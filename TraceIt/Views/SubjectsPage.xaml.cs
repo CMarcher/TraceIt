ee0237a75926b1ac9968a034102105c0effaa9a7
CMarcher <calebandcorey@gmail.com>
2020-08-25 22:04:49 +1200

    Removed ability to directly select subject item. Setting the CurrentSubject to the subject clicked on.

--- TraceIt/Views/SubjectsPage.xaml.cs
+++ TraceIt/Views/SubjectsPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Syncfusion.XForms.Buttons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Models;
+using TraceIt.Utilities;
 using TraceIt.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -25,6 +27,9 @@ namespace TraceIt.Views
 
         private void buttonViewInfo_Clicked(object sender, EventArgs e)
         {
+            var subject = (sender as SfButton)?.BindingContext as Subject;
+            StatusTracker.CurrentSubject = subject;
+
             Navigation.PushAsync(new StandardSelectionPage());
         }
     }
