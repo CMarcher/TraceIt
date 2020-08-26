4fd3ac32c15957b0d2a4f4c6df21f4ecb0891be9
Caleb <calebandcorey@gmail.com>
2020-08-26 20:03:30 +1200

    Added PropertyChanged implementation for Standards: created private backing field and manual getter & setter.

--- TraceIt/ViewModels/SelectedStandardsPageViewModel.cs
+++ TraceIt/ViewModels/SelectedStandardsPageViewModel.cs
@@ -11,15 +11,27 @@ namespace TraceIt.ViewModels
 {
     public class SelectedStandardsPageViewModel : BaseViewModel
     {
-        public ObservableCollection<AssessmentStandard> Standards = new ObservableCollection<AssessmentStandard>();
+        private ObservableCollection<AssessmentStandard> _standards;
+        public ObservableCollection<AssessmentStandard> Standards
+        {
+            get { return _standards; }
+            set
+            {
+                _standards = value;
+                OnPropertyChanged(nameof(Standards));
+            }
+        }
 
         public SelectedStandardsPageViewModel()
         {
+            Standards = new ObservableCollection<AssessmentStandard>();
+
             Task.Run(SetStandards).Wait();
 
             MessagingCenter.Subscribe<StandardCategorisedDetailPageViewModel>(this, "Update standards", (sender) =>
             {
                 Task.Run(SetStandards);
+                MessagingCenter.Send(this, "Update items source");
             });
         }
 
