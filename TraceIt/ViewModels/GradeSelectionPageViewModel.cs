86f336c23836d33236509241792f6b7b5ebed8c4
Caleb Archer <calebandcorey@gmail.com>
2020-08-28 10:26:11 +1200

    Created GradeSelectionPageViewModel and set to inherit from BaseViewModel.

---
+++ TraceIt/ViewModels/GradeSelectionPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.ViewModels
+{
+    public class GradeSelectionPageViewModel : BaseViewModel
+    {
+    }
+}
