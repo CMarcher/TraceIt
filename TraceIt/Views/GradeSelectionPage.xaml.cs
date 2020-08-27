22bab27fc865c55cde6e30dec862b66b6f4f5a8e
Caleb Archer <calebandcorey@gmail.com>
2020-08-27 12:14:25 +1200

    Set Title to include the current subject name. Set the closeToolbarItem clciked event to pop the current page.

--- TraceIt/Views/GradeSelectionPage.xaml.cs
+++ TraceIt/Views/GradeSelectionPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TraceIt.Utilities;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,12 @@ namespace TraceIt.Views
         public GradeSelectionPage()
         {
             InitializeComponent();
+            Title = "Select grades for " + StatusTracker.CurrentSubject.Name;
+        }
+
+        private async void closeToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
