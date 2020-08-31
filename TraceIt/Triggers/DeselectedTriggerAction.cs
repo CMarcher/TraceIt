45bb2075bae4d0c2fbe964c9c7fc8796fbea1ca7
CMarcher <calebandcorey@gmail.com>
2020-08-31 19:40:24 +1200

    Added setting of gradient background, the icon image source. Added Start and EndColor properties. Removed redundant appearance setting.

--- TraceIt/Triggers/DeselectedTriggerAction.cs
+++ TraceIt/Triggers/DeselectedTriggerAction.cs
@@ -7,13 +7,32 @@ namespace TraceIt.Triggers
 {
     public class DeselectedTriggerAction : TriggerAction<VisualElement>
     {
+        public Color StartColor { get; set; } = Color.Orange;
+        public Color EndColor { get; set; } = Color.LightYellow;
+
         protected override void Invoke(VisualElement sender)
         {
             var button  = sender as Button;
 
-            button.BackgroundColor = Color.Yellow;
-            button.TextColor = Color.Black;
-            button.Text = "Add";
+            button.Background = new LinearGradientBrush()
+            {
+                StartPoint = new Point(0.5, 0),
+                EndPoint = new Point(0.5, 1),
+                GradientStops = new GradientStopCollection()
+                {
+                    new GradientStop() { Offset = 0, Color = StartColor },
+                    new GradientStop() { Offset = 1, Color = EndColor }
+                }
+            };
+
+            var icon = new FontImageSource()
+            {
+                FontFamily = "PrimaryIcons",
+                Glyph = "\ue802",
+                Color = Color.Black
+            };
+
+            button.ImageSource = icon;
         }
     }
 }
--- TraceIt/Triggers/SelectedTriggerAction.cs
+++ TraceIt/Triggers/SelectedTriggerAction.cs
@@ -7,13 +7,32 @@ namespace TraceIt.Triggers
 {
     public class SelectedTriggerAction : TriggerAction<VisualElement>
     {
+        public Color StartColor { get; set; } = Color.Red;
+        public Color EndColor { get; set; } = Color.DarkRed;
+
         protected override void Invoke(VisualElement sender)
         {
             var button = sender as Button;
 
-            button.BackgroundColor = Color.Red;
-            button.Text = "Remove";
-            button.TextColor = Color.White;
+            button.Background = new LinearGradientBrush()
+            {
+                StartPoint = new Point(0.5, 0),
+                EndPoint = new Point(0.5, 1),
+                GradientStops = new GradientStopCollection()
+                {
+                    new GradientStop() { Offset = 0, Color = StartColor },
+                    new GradientStop() { Offset = 1, Color = EndColor }
+                }
+            };
+
+            var icon = new FontImageSource()
+            {
+                FontFamily = "PrimaryIcons",
+                Glyph = "\uf1f8",
+                Color = Color.White
+            };
+
+            button.ImageSource = icon;
         }
     }
 }
