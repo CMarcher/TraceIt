aca16e085f857aa7ba8f8d0fc3c65e16a2502a89
Caleb Archer <calebandcorey@gmail.com>
2020-09-03 12:20:06 +1200

    Added SaveData bool to determine whether database should be reset or not.

--- TraceIt/App.xaml.cs
+++ TraceIt/App.xaml.cs
@@ -13,6 +13,7 @@ namespace TraceIt
     {
         public static NavigationService NavigationService { get; } = new NavigationService();
         public static DataService DataService { get; } = new DataService();
+        public static bool SaveData { get; private set; } = false;
 
         public App()
         {
