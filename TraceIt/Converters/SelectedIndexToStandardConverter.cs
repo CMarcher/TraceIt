9a5e828e2389596b328c8ada62fb0e87ee7803ef
Caleb Archer <calebandcorey@gmail.com>
2020-09-03 12:16:18 +1200

    Created SelectedIndexToStandardConverter to convert the selected index of goal grade selector to grade enum option.

---
+++ TraceIt/Converters/SelectedIndexToStandardConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using TraceIt.Models;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class SelectedIndexToStandardConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((int)value)
+            {
+                case 0:
+                    return Standard.Grade.NotAchieved;
+                case 1:
+                    return Standard.Grade.Achieved;
+                case 2:
+                    return Standard.Grade.Merit;
+                case 3:
+                    return Standard.Grade.Excellence;
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch((Standard.Grade)value)
+            {
+                case Standard.Grade.NotAchieved:
+                    return 0;
+                case Standard.Grade.Achieved:
+                    return 1;
+                case Standard.Grade.Merit:
+                    return 2;
+                case Standard.Grade.Excellence:
+                    return 3;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
