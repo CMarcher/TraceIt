82179d21d30ac62cca4845d8c4dc652caa6bfb17
Caleb Archer <calebandcorey@gmail.com>
2020-09-03 12:17:57 +1200

    Added Standard property, set to CurrentStandard in constructor.

--- TraceIt/ViewModels/GradeSelectionPageViewModel.cs
+++ TraceIt/ViewModels/GradeSelectionPageViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TraceIt.Models;
+using TraceIt.Utilities;
 
 namespace TraceIt.ViewModels
 {
     public class GradeSelectionPageViewModel : BaseViewModel
     {
+        public Standard Standard { get; private set; }
+
+        public GradeSelectionPageViewModel()
+        {
+            Standard = StatusTracker.CurrentStandard;
+        }
     }
 }
