da2e8d84ef365375d8c1eb577d6c080169e62e61
CMarcher <calebandcorey@gmail.com>
2020-09-06 19:51:48 +1200

    Created IsToggledToMaximumConverter that returns different maximum values for endorsement progress bars depending on whether a switch is on

---
+++ TraceIt/Converters/IsToggledToMaximumConverter.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.GridCommon;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class IsToggledToMaximumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int normalValue = (string)parameter == "Subject" ? 14 : 50;
+            int covidValue = (string)parameter == "Subject" ? 12 : 46;
+            bool isCovidSetting = (bool)value;
+
+            if (isCovidSetting)
+                return covidValue;
+            else
+                return normalValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
