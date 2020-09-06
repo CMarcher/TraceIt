97b270910635393d86d1505dd10a0a2cc02df664
CMarcher <calebandcorey@gmail.com>
2020-09-06 19:50:08 +1200

    Created GradeEnumToLetter converter that takes an enum value and returns a single letter representing that value.

---
+++ TraceIt/Converters/GradeEnumToLetterConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using TraceIt.Models;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class GradeEnumToLetterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((Standard.Grade)value)
+            {
+                case Standard.Grade.NotAchieved:
+                    return "NA";
+                case Standard.Grade.Achieved:
+                    return "A";
+                case Standard.Grade.Merit:
+                    return "M";
+                case Standard.Grade.Excellence:
+                    return "E";
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
