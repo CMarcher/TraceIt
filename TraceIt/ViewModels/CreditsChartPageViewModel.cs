40b22d57e8dfb9166a3d3059f05afdda67565405
CMarcher <calebandcorey@gmail.com>
2020-09-07 22:10:45 +1200

    Created CreditsChartPageViewModel and relevant properties. Created SetCredits method that sets each of the credit properties. Called in constructor and messaging center subscription.

---
+++ TraceIt/ViewModels/CreditsChartPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Views;
+using Xamarin.Forms;
+
+namespace TraceIt.ViewModels
+{
+    public class CreditsChartPageViewModel : BaseViewModel
+    {
+        private int _totalCredits;
+        public int TotalCredits
+        {
+            get { return _totalCredits; }
+            set
+            {
+                _totalCredits = value;
+                OnPropertyChanged(nameof(TotalCredits));
+            }
+        }
+
+        private int _achievedCredits;
+        public int AchievedCredits
+        {
+            get { return _achievedCredits; }
+            set
+            {
+                _achievedCredits = value;
+                OnPropertyChanged(nameof(AchievedCredits));
+            }
+        }
+
+        public CreditsChartPageViewModel()
+        {
+            Task.Run(SetCredits).Wait();
+
+            MessagingCenter.Subscribe<GradeSelectionPage>(this, "Update standard",
+                async (sender) => await SetCredits());
+        }
+
+        async Task SetCredits()
+        {
+            var credits = await App.DataService.GetAchievedAndTotalCreditsAsync();
+            TotalCredits = credits.Item1;
+            AchievedCredits = credits.Item2;
+        }
+    }
+}
