79f84d7d2463577b31ac9c1fc1319cbbf0f6c1d3
CMarcher <calebandcorey@gmail.com>
2020-09-08 21:25:28 +1200

    Created MessagingService to manage all MessagingCenter message sending and subscribing.

---
+++ TraceIt/Services/MessagingService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Services
+{
+    public class MessagingService
+    {
+        string UpdateStandardMessage = "Update standard";
+
+        public enum MessageType
+        {
+            UpdateStandard
+        }
+
+        public MessagingService()
+        {
+
+        }
+
+        public void Send(MessageType message) 
+            => MessagingCenter.Send(this, GetMessage(message));
+        
+
+        public void Subscribe(object subscriber, MessageType message, Action<MessagingService> action)
+            => MessagingCenter.Subscribe(subscriber, GetMessage(message), action);
+        
+
+        public string GetMessage(MessageType message)
+        {
+            string finalMessage;
+
+            switch (message)
+            {
+                case MessageType.UpdateStandard:
+                    finalMessage = UpdateStandardMessage;
+                    break;
+                default:
+                    finalMessage = null;
+                    break;
+            }
+
+            return finalMessage;
+        }
+    }
+}
