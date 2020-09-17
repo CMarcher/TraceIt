f1aee3b4a77d4883caf1344e60bc29611d05598c
CMarcher <calebandcorey@gmail.com>
2020-09-18 06:46:16 +1200

    Created TotalCreditsToSecondaryProgressConverter to convert merit and excellence values to be displayed.

---
+++ TraceIt/Converters/TotalCreditsToSecondaryProgressConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class TotalCreditsToSecondaryProgressConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is null || values[1] is null)
+                return 0;
+
+            int firstValue = (int)values[0], secondValue = (int)values[1];
+            return firstValue + secondValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
