3053a35f2e2bc29cb816645f2268ec96ca9ea9c2
CMarcher <calebandcorey@gmail.com>
2020-09-18 06:58:02 +1200

    Changed Standard to private set. Updated use of PushChangesAsync.

--- TraceIt/ViewModels/GradeSelectionPageViewModel.cs
+++ TraceIt/ViewModels/GradeSelectionPageViewModel.cs
@@ -13,16 +13,17 @@ namespace TraceIt.ViewModels
 {
     public class GradeSelectionPageViewModel : BaseViewModel
     {
-        public Standard Standard { get; set; }
+        public Standard Standard { get; private set; }
 
         public GradeSelectionPageViewModel()
         {
             Standard = StatusTracker.CurrentStandard;
 
-            App.MessagingService.Subscribe(this, MessagingService.MessageType.PushStandard, (sender) =>
+            App.MessagingService.Subscribe(this, MessagingService.MessageType.PushStandard,
+                (sender) =>
                 {
-                    Task.Run(Standard.PushChangesAsync).Wait();
-                    App.MessagingService.Send(MessagingService.MessageType.RefreshStandards);
+                    Task.Run(() => Standard.PushChangesAsync(false));
+                    App.MessagingService.Send(MessagingService.MessageType.RefreshEndorsements);
                 });
         }
 
