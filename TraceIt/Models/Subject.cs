14c685f3328b6b99271c5944e3a60c7f0940ba50
CMarcher <calebandcorey@gmail.com>
2020-09-29 16:51:51 +1300

    Reformatted property notification setting and getting to be cleaner, using lambdas. Added Credits, Merit & ExcellenceCredits properties.

--- TraceIt/Models/Subject.cs
+++ TraceIt/Models/Subject.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TraceIt.Models
 {
@@ -14,53 +15,66 @@ namespace TraceIt.Models
         [PrimaryKey]
         public int ID
         {
-            get { return _id; }
-            set
-            {
-                _id = value;
-                OnPropertyChanged(nameof(ID));
-            }
+            get => _id;
+            set => SetProperty(ref _id, value, ID);
         }
 
         private string _name;
         [NotNull]
         public string Name
         {
-            get { return _name; }
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
+            get => _name;
+            set => SetProperty(ref _name, value, Name);
         }
 
         [NotNull]
         public string Subfield { get; set; }
 
+        private int _credits;
+        public int Credits
+        {
+            get => _credits;
+            set => SetProperty(ref _credits, value, Credits);
+        }
+
+        private int _meritCredits;
+        public int MeritCredits
+        {
+            get => _meritCredits;
+            set => SetProperty(ref _meritCredits, value, MeritCredits);
+        }
+
+        private int _excellenceCredits;
+        public int ExcellenceCredits
+        {
+            get => _excellenceCredits;
+            set => SetProperty(ref _excellenceCredits, value, ExcellenceCredits);
+        }
+
         private bool _selected;
         [NotNull]
         public bool Selected
         {
-            get { return _selected; }
-            set
-            {
-                _selected = value;
-                OnPropertyChanged(nameof(Selected));
-            }
+            get => _selected;
+            set => SetProperty(ref _selected, value, Selected);
         }
 
         private bool _custom;
         [NotNull]
         public bool Custom
         {
-            get { return _custom; }
-            set
-            {
-                _custom = value;
-                OnPropertyChanged(nameof(_custom));
-            }
+            get => _custom;
+            set => SetProperty(ref _custom, value, Custom);
         }
 
         public Subject() { }
+
+        public async Task PushChanges()
+            => await App.DataService.UpdateSubjectAsync(this);
+        
+        //async Task ResetSubject()
+        //{
+
+        //}
     }
 }
