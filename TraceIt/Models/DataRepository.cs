0fab0d0b2122c1edcf6081c39d1e8511b6f68a6a
CMarcher <calebandcorey@gmail.com>
2020-09-29 16:49:09 +1300

    Created DataRepository class and created some methods to initialise selected subjects and set their credits.

---
+++ TraceIt/Models/DataRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceIt.Models
+{
+    public class DataRepository : BaseModel
+    {
+        public ObservableCollection<Subject> SelectedSubjects = new ObservableCollection<Subject>();
+
+        public DataRepository()
+        {
+            Task.Run(InitialiseSubjects).Wait();
+        }
+
+        async Task InitialiseSubjects()
+        {
+            SelectedSubjects = await App.DataService.GetSelectedSubjectsAsync();
+
+            foreach (var subject in SelectedSubjects)
+            {
+                var standards = await App.DataService.GetStandardsForSubjectAsync(subject.Name);
+                foreach (var standard in standards)
+                {
+                    SetSubjectCredits(subject, standard);
+                }
+
+                await subject.PushChanges();
+            }
+        }
+
+        void SetSubjectCredits(Subject subject, Standard standard)
+        {
+            if (standard.FinalGrade == Standard.Grade.Excellence)
+                subject.ExcellenceCredits += standard.Credits;
+            else if (standard.FinalGrade == Standard.Grade.Merit)
+                subject.MeritCredits += standard.Credits;
+
+            subject.Credits += standard.Credits;
+        }
+    }
+}
