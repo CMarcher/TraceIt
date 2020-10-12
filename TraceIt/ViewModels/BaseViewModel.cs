6a51f402a809e57ce246bc2478ee80c7b0ff531e
CMarcher <calebandcorey@gmail.com>
2020-10-12 18:09:17 +1300

    Added SetProperty method that takes the property and new value, setting them, as well as the public property name for throwing the PropertyChanged event.

--- TraceIt/ViewModels/BaseViewModel.cs
+++ TraceIt/ViewModels/BaseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace TraceIt.ViewModels
@@ -12,6 +13,12 @@ namespace TraceIt.ViewModels
         protected virtual void OnPropertyChanged(string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SetProperty<T>(ref T property, T value, string propertyName)
+        {
+            property = value;
+            OnPropertyChanged(propertyName);
         }
 
         public virtual void Initialize(object parameter)
