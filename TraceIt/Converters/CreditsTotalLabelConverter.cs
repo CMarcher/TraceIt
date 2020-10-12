299cb75f413ec4fe1b756d00fbccf5d2d6971994
CMarcher <calebandcorey@gmail.com>
2020-10-12 17:30:11 +1300

    Created CreditsTotalLabelConverter that takes two credit-based values and returns a string.

---
+++ TraceIt/Converters/CreditsTotalLabelConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    class CreditsTotalLabelConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is null || values[1] is null)
+                return null;
+
+            var passedCredits = (int)values[0];
+            var totalCredits = (int)values[1];
+
+            string result = passedCredits + " / " + totalCredits + " credits";
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
