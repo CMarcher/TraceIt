76d2feebf46808aaabae20ee760b8aed9eb90bfe
CMarcher <calebandcorey@gmail.com>
2020-10-12 17:58:40 +1300

    Created SubjectEndorsement class that inherits from Endorsement, with one additional Name property.

---
+++ TraceIt/Models/SubjectEndorsement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models
+{
+    public class SubjectEndorsement : Endorsement
+    {
+        public string Name { get; set; }
+    }
+}
