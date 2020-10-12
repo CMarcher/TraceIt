d7187321c906f0ef6b20b303f6ab6ab658f7590a
CMarcher <calebandcorey@gmail.com>
2020-10-12 18:36:02 +1300

    Reformatted Subjects to use property notification pattern. Created InitialiseAsync method that initialises the app's data repository and creates and returns the corresponding view model. Created SetCommands method. Created RemoveSubject method used by RemoveSubjectCommand.

--- TraceIt/ViewModels/SubjectsPageViewModel.cs
+++ TraceIt/ViewModels/SubjectsPageViewModel.cs
@@ -2,24 +2,47 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Models;
+using Xamarin.Forms;
 
 namespace TraceIt.ViewModels
 {
     public class SubjectsPageViewModel : BaseViewModel
     {
-        public ObservableCollection<Subject> Subjects { get; private set; }
+        private ObservableCollection<Subject> _subjects;
+        public ObservableCollection<Subject> Subjects
+        {
+            get => _subjects;
+            set => SetProperty(ref _subjects, value, nameof(Subjects));
+        }
+
+        public Command RemoveSubjectCommand { get; private set; }
 
         public SubjectsPageViewModel()
         {
             SetSubjects();
+            SetCommands();
+        }
+
+        public static async Task<SubjectsPageViewModel> InitialiseAsync()
+        {
+            await Task.Run(App.DataRepository.InitialiseAsync);
+            var viewmodel = new SubjectsPageViewModel();
+            return viewmodel;
         }
 
         private void SetSubjects()
             => Subjects = App.DataRepository.SelectedSubjects;
+
+        private void SetCommands()
+            => RemoveSubjectCommand = new Command<Subject>(async (subject) => await RemoveSubject(subject));
+        
+        private async Task RemoveSubject(Subject subject)
+            => await App.DataRepository.RemoveSubject(subject);
 
     }
 }
