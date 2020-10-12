b99710fbee15bb00f669d90d0f3080457543f219
CMarcher <calebandcorey@gmail.com>
2020-10-12 19:05:18 +1300

    Set BindingContext in OnAppearing. Created SetContext method used in OnAppearing.

--- TraceIt/Views/SubjectsPage.xaml.cs
+++ TraceIt/Views/SubjectsPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Syncfusion.XForms.Buttons;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using TraceIt.Models;
@@ -13,7 +16,7 @@ using Xamarin.Forms.Xaml;
 namespace TraceIt.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SubjectsPage : ContentPage
+    public partial class SubjectsPage : BasePage
     {
         public SubjectsPage()
         {
@@ -22,10 +25,27 @@ namespace TraceIt.Views
 
         private async void buttonViewInfo_Clicked(object sender, EventArgs e)
         {
-            var subject = (sender as SfButton)?.BindingContext as Subject;
+            var subject = (sender as Button)?.BindingContext as Subject;
             StatusTracker.CurrentSubject = subject;
 
-            await Navigation.PushAsync(new StandardSelectionPage());
+            await Navigation.PushAsync(new SelectedStandardsPage());
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (Initialised is false)
+                await SetContext();
+
+            loadingView.IsVisible = false;
+            loadingView.IsRunning = false;
+        }
+
+        async Task SetContext()
+        {
+            BindingContext = await SubjectsPageViewModel.InitialiseAsync();
+            Initialised = true;
         }
     }
 }
