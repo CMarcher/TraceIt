46c6bcee3663d85fdd03568f246a70bbd629ce88
CMarcher <calebandcorey@gmail.com>
2020-10-12 17:35:54 +1300

    Created CreditBreakdown class which Grades and Credits properties.

---
+++ TraceIt/Models/Query Models/CreditBreakdown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Models.Query_Models
+{
+    public class CreditBreakdown
+    {
+        public enum Grades
+        {
+            NotAchieved,
+            Achieved,
+            Merit,
+            Excellence
+        }
+
+        public int Credits { get; set; }
+        public Grades Grade { get; set; }
+        
+    }
+}
