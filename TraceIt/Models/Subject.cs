2872d8bff2006cad479327b242d87bd92c1843bf
CMarcher <calebandcorey@gmail.com>
2020-10-12 17:57:42 +1300

    Restored nameof() usage due to issues with passing property directly. Created PassedCredits and StandardsCount properties. Added "Async" to PushChanges method. Created RrefreshAsync method that resets dynamic properties and pushes the changes. Created Delete method that deselects the subject or removes it entirely based on the value of Custom. Added SetCredits, AddStandardCredits methods. Added Standards property that holds a list of standards added to the subject. Added InitialiseStandards, AddStandardAsync, RemoveStandardAsync and ClearStandards methods. Added SubscribeToFinalChanged, UnsubscribeFromFinalGradeChanged and OnFinalGradeChanged methods that manage subscribing to the changing of each standards final grade. Added SetPassedCredits that sets PassedCredits to the total credits from standards that at least pass.

--- TraceIt/Models/Subject.cs
+++ TraceIt/Models/Subject.cs
@@ -1,22 +1,25 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TraceIt.Extensions;
 
 namespace TraceIt.Models
 {
     [Table("Subjects")]
-    public class Subject : BaseModel, ISubjectItem 
+    public class Subject : BaseModel, ISubjectItem
     {
         private int _id;
         [PrimaryKey]
         public int ID
         {
             get => _id;
-            set => SetProperty(ref _id, value, ID);
+            set => SetProperty(ref _id, value, nameof(ID));
         }
 
         private string _name;
@@ -24,31 +27,42 @@ namespace TraceIt.Models
         public string Name
         {
             get => _name;
-            set => SetProperty(ref _name, value, Name);
+            set => SetProperty(ref _name, value, nameof(Name));
         }
 
         [NotNull]
         public string Subfield { get; set; }
 
         private int _credits;
+        [NotNull]
         public int Credits
         {
             get => _credits;
-            set => SetProperty(ref _credits, value, Credits);
+            set => SetProperty(ref _credits, value, nameof(Credits));
+        }
+
+        private int _passedCredits;
+        [NotNull]
+        public int PassedCredits
+        {
+            get => _passedCredits;
+            set => SetProperty(ref _passedCredits, value, nameof(PassedCredits));
         }
 
         private int _meritCredits;
+        [NotNull]
         public int MeritCredits
         {
             get => _meritCredits;
-            set => SetProperty(ref _meritCredits, value, MeritCredits);
+            set => SetProperty(ref _meritCredits, value, nameof(MeritCredits));
         }
 
         private int _excellenceCredits;
+        [NotNull]
         public int ExcellenceCredits
         {
             get => _excellenceCredits;
-            set => SetProperty(ref _excellenceCredits, value, ExcellenceCredits);
+            set => SetProperty(ref _excellenceCredits, value, nameof(ExcellenceCredits));
         }
 
         private bool _selected;
@@ -56,7 +70,7 @@ namespace TraceIt.Models
         public bool Selected
         {
             get => _selected;
-            set => SetProperty(ref _selected, value, Selected);
+            set => SetProperty(ref _selected, value, nameof(Selected));
         }
 
         private bool _custom;
@@ -64,17 +78,117 @@ namespace TraceIt.Models
         public bool Custom
         {
             get => _custom;
-            set => SetProperty(ref _custom, value, Custom);
+            set => SetProperty(ref _custom, value, nameof(Custom));
         }
+
+        private int _standardsCount;
+        [Column("Standards")]
+        public int StandardsCount
+        {
+            get => _standardsCount;
+            set => SetProperty(ref _standardsCount, value, nameof(StandardsCount));
+        }
+
+        void CountStandards()
+            => StandardsCount = Standards.Count;
 
         public Subject() { }
 
-        public async Task PushChanges()
+        public async Task PushChangesAsync()
             => await App.DataService.UpdateSubjectAsync(this);
-        
-        //async Task ResetSubject()
-        //{
 
-        //}
+        private async Task RefreshAsync()
+        {
+            SetCredits();
+            CountStandards();
+            await PushChangesAsync();
+        }
+
+        public async Task Delete()
+        {
+            await ClearStandards();
+
+            if (Custom is false)
+            {
+                Selected = false;
+                Credits = MeritCredits = ExcellenceCredits = 0;
+                await PushChangesAsync();
+            }
+            else
+                await App.DataService.DeleteSubjectAsync(this);
+        }
+
+        private void SetCredits()
+        {
+            Credits = 0;
+
+            foreach (var standard in Standards)
+                AddStandardCredits(standard);
+        }
+
+        private void AddStandardCredits(Standard standard)
+        {
+            if (standard.FinalGrade is Standard.Grade.Excellence)
+                ExcellenceCredits += standard.Credits;
+            else if (standard.FinalGrade is Standard.Grade.Merit)
+                MeritCredits += standard.Credits;
+
+            Credits += standard.Credits;
+        }
+
+        [Ignore]
+        public ObservableCollection<Standard> Standards { get; set; } = new ObservableCollection<Standard>();
+
+        public async Task InitialiseStandards()
+        {
+            Standards = await App.DataService.GetStandardsForSubjectAsync(Name);
+            await RefreshAsync();
+
+            foreach (var standard in Standards)
+                SubscribeToFinalGradeChanged(standard);
+
+            await SetPassedCredits();
+        }
+
+        public async Task AddStandardAsync(Standard standard)
+        {
+            Standards.Add(standard);
+            await standard.SelectAsync(this);
+            await RefreshAsync();
+            SubscribeToFinalGradeChanged(standard);
+        }
+
+        public async Task RemoveStandardAsync(Standard standard)
+        {
+            Standards.RemoveByID(standard);
+            await standard.DeselectAsync();
+            await RefreshAsync();
+            UnsubscribeFromFinalGradeChanged(standard);
+        }
+
+        private async Task ClearStandards()
+        {
+            foreach (var standard in Standards)
+            {
+                UnsubscribeFromFinalGradeChanged(standard);
+                await standard.DeselectAsync();
+            }
+        }
+
+        private void SubscribeToFinalGradeChanged(Standard standard)
+            => standard.FinalGradeChanged += new EventHandler(OnFinalGradeChanged);
+
+        private void UnsubscribeFromFinalGradeChanged(Standard standard)
+            => standard.FinalGradeChanged -= OnFinalGradeChanged;
+
+        private async void OnFinalGradeChanged(object sender, EventArgs e)
+            => await SetPassedCredits();
+
+        private async Task SetPassedCredits()
+        {
+            PassedCredits = Standards.CountCredits(standard => (int)standard.FinalGrade >= 1);
+            await PushChangesAsync();
+        }
+
     }
 }
