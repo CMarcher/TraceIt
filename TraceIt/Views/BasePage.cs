225ae017d80611fe0c6e2b2d3ebb66f7d4aa5be0
CMarcher <calebandcorey@gmail.com>
2020-10-12 18:44:07 +1300

    Created BasePage class that inherits from ContentPage, with an additional Initialised property.

---
+++ TraceIt/Views/BasePage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Views
+{
+    public abstract class BasePage : ContentPage
+    {
+        public bool Initialised { get; set; }
+    }
+}
