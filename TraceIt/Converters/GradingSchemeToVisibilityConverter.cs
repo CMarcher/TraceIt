ec39f319c1d55aec636998c651ff1a82547e6a20
Caleb Archer <calebandcorey@gmail.com>
2020-10-14 09:43:55 +1300

    Created GradingSchemeToVisibilityConverter and implemented Convert method, taking the source grading scheme and the parameter, returning a bool value.

---
+++ TraceIt/Converters/GradingSchemeToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using TraceIt.Models;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class GradingSchemeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segmentType = (string)parameter;
+
+            bool IsAchievedOnly = (Standard.GradeOptions)value == Standard.GradeOptions.AchievedOnly;
+            bool IsUpToMerit = (Standard.GradeOptions)value == Standard.GradeOptions.UpToMerit;
+            bool IsUpToExcellence = (Standard.GradeOptions)value == Standard.GradeOptions.UpToExcellence;
+
+            bool IsMeritItem = segmentType == "MeritItem";
+            bool IsExcellenceItem = segmentType == "ExcellenceItem";
+
+            if (IsAchievedOnly && IsMeritItem)
+                return false;
+            else if (IsAchievedOnly && IsExcellenceItem)
+                return false;
+
+            else if (IsUpToMerit && IsMeritItem)
+                return true;
+            else if (IsUpToMerit && IsExcellenceItem)
+                return false;
+
+            else if (IsUpToExcellence)
+                return true;
+            else
+                return false;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
