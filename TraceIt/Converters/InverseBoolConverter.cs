e77e3d9fffae4bccdf2b0504d338572bad69cb6e
Caleb Archer <calebandcorey@gmail.com>
2020-10-15 12:15:50 +1300

    Created InverseBoolConverter to invert the result of a boolean value.

---
+++ TraceIt/Converters/InverseBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = (bool)value;
+
+            if (result is true)
+                return false;
+            else
+                return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
