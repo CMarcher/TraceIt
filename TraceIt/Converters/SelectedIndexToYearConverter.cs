548bd99511d26b650e0e5b94d8e623d5bb72feb3
CMarcher <calebandcorey@gmail.com>
2020-10-21 22:30:08 +1300

    Implemented SelectedIndexToYearConverter Convert and ConvertBack methods, with additional helper methods GetIndex and GetYear.

--- TraceIt/Converters/SelectedIndexToYearConverter.cs
+++ TraceIt/Converters/SelectedIndexToYearConverter.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace TraceIt.Converters
 {
-    class SelectedIndexToYearConverter
+    public class SelectedIndexToYearConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int year = (int)value;
+
+            return GetIndex(year);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int index = (int)value;
+
+            return GetYear(index);
+        }
+
+        private int GetIndex(int year)
+        {
+            if (year is 2017)
+                return 0;
+            if (year is 2018)
+                return 1;
+            if (year is 2019)
+                return 2;
+            if (year is 2020)
+                return 3;
+            else
+                throw new Exception("Invalid year: " + year);
+        }
+
+        private int GetYear(int index)
+        {
+            try
+            {
+                if (index is 0)
+                    return 2017;
+                if (index is 1)
+                    return 2018;
+                if (index is 2)
+                    return 2019;
+                if (index is 3)
+                    return 2020;
+                else
+                    throw new Exception("Invalid index: " + index);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 2020;
+            }
+        }
     }
 }
