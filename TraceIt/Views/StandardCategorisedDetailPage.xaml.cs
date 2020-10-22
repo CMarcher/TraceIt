604dbd5ead43b799612d091d9b0b73deed45c914
CMarcher <calebandcorey@gmail.com>
2020-10-23 00:18:05 +1300

    Moved level SfSegmentedControl to StandardCategorisedDetailPage. Implemented SelectedIndex changed which changes the filtering based on year. Added Initialise and InitialiseOrRefreshDataSource methods. Added FilterStandards and GetLevelFromSelectedIndex methods.

--- TraceIt/Views/StandardCategorisedDetailPage.xaml.cs
+++ TraceIt/Views/StandardCategorisedDetailPage.xaml.cs
@@ -21,16 +21,57 @@ namespace TraceIt.Views
         public StandardCategorisedDetailPage(string parameter, DataService.FilterOption filterByOption)
         {
             InitializeComponent();
+            Initialise(parameter, filterByOption);
+        }
+
+        private void Initialise(string parameter, DataService.FilterOption filterByOption)
+        {
             ViewModel = new StandardCategorisedDetailPageViewModel(parameter, filterByOption);
             BindingContext = ViewModel;
 
             Title = parameter;
             categorisedStandardsList.ItemsSource = ViewModel.Standards;
+            InitialiseOrRefreshDataSource();
+        }
+
+        private void InitialiseOrRefreshDataSource()
+        {
+            categorisedStandardsList.DataSource.Filter = FilterStandards;
+            categorisedStandardsList.DataSource.Refresh();
+            categorisedStandardsList.DataSource.RefreshFilter();
         }
 
         private void categorisedStandardsList_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
         {
             Navigation.PushAsync(new StandardDetailPage(e.ItemData as Standard));
         }
+
+        private bool FilterStandards(object item)
+        {
+            if (categorisedStandardsList.DataSource is null)
+                return true;
+
+            var standard = item as Standard;
+            bool matchesSelectedLevel = standard.Level == GetLevelFromIndex();
+
+            return matchesSelectedLevel;
+        }
+
+        private Standard.Levels GetLevelFromIndex()
+        {
+            int index = levelSelector.SelectedIndex;
+
+            if (index is 0)
+                return Standard.Levels.One;
+            else if (index is 1)
+                return Standard.Levels.Two;
+            else if (index is 2)
+                return Standard.Levels.Three;
+            else
+                return Standard.Levels.One;
+        }
+
+        private void levelSelector_SelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e)
+            => InitialiseOrRefreshDataSource();
     }
 }
