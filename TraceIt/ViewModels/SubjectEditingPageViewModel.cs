360d1bc7889ba83ca4e8b778be2bb103840b96d5
CMarcher <calebandcorey@gmail.com>
2020-10-23 00:11:38 +1300

    Created SubjectEditingPageViewModel to work with new SubjectEditingPage. Pretty much a cousin to SubjectSelectionPageViewModel.

---
+++ TraceIt/ViewModels/SubjectEditingPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Models;
+using Xamarin.Forms;
+
+namespace TraceIt.ViewModels
+{
+    public class SubjectEditingPageViewModel : BaseViewModel
+    {
+        private ObservableCollection<SelectedSubject> _subjects;
+        public ObservableCollection<SelectedSubject> Subjects
+        {
+            get => _subjects;
+            set => SetProperty(ref _subjects, value, nameof(Subjects));
+        }
+        public Command ChangeSelectionCommand { get; private set; }
+
+        public SubjectEditingPageViewModel()
+        {
+            SetSubjectsAsync();
+            InitialiseCommands();
+        }
+
+        private void InitialiseCommands()
+            => ChangeSelectionCommand = new Command<SelectedSubject>(async (subject) => await ChangeSubjectSelection(subject));
+
+        private async Task ChangeSubjectSelection(SelectedSubject subject)
+        {
+            Task task = subject.Selected == false ?
+                subject.Select() :
+                subject.Deselect();
+
+            await task;
+        }
+
+        private void SetSubjectsAsync() => Subjects = App.DataRepository.SelectedSubjects;
+    }
+}
