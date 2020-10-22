d75dce81e850dd4e09ee7aa6c4f6dd6d46a9961f
CMarcher <calebandcorey@gmail.com>
2020-10-23 00:20:24 +1300

    Created SubjectEditingPage, mostly based on SubjectSelectionPage, but behaviour and some appearances altered to consist with its purpose. Added TapGestureRecognizer_Tapped that handles changing selection. If being deslected, it asks the user to confirm first.

---
+++ TraceIt/Views/SubjectEditingPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraceIt.Models;
+using TraceIt.Utilities;
+using TraceIt.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TraceIt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubjectEditingPage : ContentPage
+    {
+        public SubjectEditingPage()
+        {
+            InitializeComponent();
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            Title = "Select Subjects for " + StatusTracker.CurrentYear;
+            RefreshDatasource();
+        }
+
+        void RefreshDatasource()
+        {
+            subjectsListView.DataSource.Filter = FilterSubjects;
+            subjectsListView.DataSource.Refresh();
+        }
+
+        private async void closeButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private void searchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (subjectsListView.DataSource != null)
+                RefreshDatasource();
+        }
+
+        private bool FilterSubjects(object item)
+        {
+            if (subjectsListView.DataSource is null)
+                return true;
+
+            var subject = item as SelectedSubject;
+            int year = StatusTracker.CurrentYear;
+            bool matchesSelectedYear = subject.Year == year;
+            bool searchCriteriaMatched;
+
+            if (searchBar.Text == null)
+                searchCriteriaMatched = true;
+            else
+                searchCriteriaMatched = subject.BaseSubject.Name.ToLower().Contains(searchBar.Text.ToLower());
+
+            return matchesSelectedYear is true && searchCriteriaMatched is true;
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var subject = (sender as Frame).BindingContext as SelectedSubject;
+            var viewmodel = BindingContext as SubjectEditingPageViewModel;
+            bool isBeingDeselected = subject.Selected is true;
+            bool deselectionConfirmed;
+
+            string title = "Subject being removed";
+            string message = "You are about to remove this subject, which will clear all standards you have added. Continue?";
+            string confirm = "Yes", cancel = "Never mind";
+
+            if (isBeingDeselected)
+            {
+                deselectionConfirmed = await DisplayAlert(title, message, confirm, cancel);
+
+                if (deselectionConfirmed)
+                    viewmodel.ChangeSelectionCommand.Execute(subject);
+            }
+            else
+                viewmodel.ChangeSelectionCommand.Execute(subject);
+        }
+    }
+}
