6028125c580d2c8314f4ca030d9e9e7bd872a30c
CMarcher <calebandcorey@gmail.com>
2020-10-27 23:38:13 +1300

    Added constructor that requires the Grade to be defined through parameter. Created new AddCreditsFromSubject method that loops through all standards and adds their credits if they match the Breakdown grade.

--- TraceIt/Models/Query Models/CreditBreakdown.cs
+++ TraceIt/Models/Query Models/CreditBreakdown.cs
@@ -16,6 +16,30 @@ namespace TraceIt.Models.Query_Models
 
         public int Credits { get; set; }
         public Grades Grade { get; set; }
+
+        public CreditBreakdown(Grades grade)
+        {
+            Grade = grade;
+        }
+
+        public void AddCreditsFromSubject(SelectedSubject subject)
+        {
+            foreach(var standard in subject.Standards)
+            {
+                bool bothNotAchieved = standard.FinalGrade == Standard.Grade.NotAchieved &&
+                                       Grade == Grades.NotAchieved;
+                bool bothAchieved = standard.FinalGrade == Standard.Grade.Achieved &&
+                                    Grade == Grades.Achieved;
+                bool bothMerit = standard.FinalGrade == Standard.Grade.Merit &&
+                                 Grade == Grades.Merit;
+                bool bothExcellence = standard.FinalGrade == Standard.Grade.Excellence &&
+                                      Grade == Grades.Excellence;
+                bool canAddCredits = bothNotAchieved || bothAchieved || bothMerit || bothExcellence;
+
+                if (canAddCredits)
+                    Credits += standard.Credits;
+            }
+        }
         
     }
 }
