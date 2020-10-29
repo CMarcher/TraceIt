0c9b93f6c6a01e1c9abadef287aadad910f62f90
CMarcher <calebandcorey@gmail.com>
2020-10-29 22:53:58 +1300

    subjectProgressBar setter for maximum removed, now defined by triggers. Set platform specific padding. New SfSegmentedControl for selecting the endorsement type (Merit & Excellence). New and existing progress bars embedded within grid with 3 columns, two new ones for the external and internal credit requirements. Altered FilterEndorsements to match model changes and filter for endorsement type too.

--- TraceIt/Views/Charts/EndorsementsChartPage.xaml.cs
+++ TraceIt/Views/Charts/EndorsementsChartPage.xaml.cs
@@ -33,20 +33,39 @@ namespace TraceIt.Views.Charts
         
         private bool FilterEndorsements(object item)
         {
-            if (item is SelectedSubject)
+            if (item is SubjectEndorsement)
             {
                 var viewmodel = BindingContext as EndorsementsChartPageViewModel;
-                var subject = item as SelectedSubject;
-                bool isSelected = subject.Selected;
-                bool matchesSelectedYear = subject.Year == viewmodel.Year;
+                var endorsementType = GetEndorsementType();
+                var endorsement = item as SubjectEndorsement;
 
-                return isSelected && matchesSelectedYear;
+                bool matchesSelectedYear = endorsement.Year == viewmodel.Year;
+                bool matchesEndorsementType = endorsement.EndorsementType == endorsementType;
+
+                return matchesSelectedYear && matchesEndorsementType;
             }
             else
                 throw new Exception("Invalid item type: " + item.GetType());
         }
 
+        private SubjectEndorsement.EndorsementTypes GetEndorsementType()
+        {
+            var index = endorsementTypeSelector.SelectedIndex;
+
+            if (index is 0)
+                return SubjectEndorsement.EndorsementTypes.Merit;
+            else if (index is 1)
+                return SubjectEndorsement.EndorsementTypes.Excellence;
+            else
+                return SubjectEndorsement.EndorsementTypes.Merit;
+        }
+
         private void SfSegmentedControl_SelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e)
+        {
+            RefreshDataSource();
+        }
+
+        private void endorsementTypeSelector_SelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e)
         {
             RefreshDataSource();
         }
