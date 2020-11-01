35e04d899b82a5e560823c14f9851e6fff194cdb
CMarcher <calebandcorey@gmail.com>
2020-11-01 22:34:48 +1300

    New work in progress CacheManagerService for platform specific cache clearing.

---
+++ TraceIt/Services/CacheManagerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TraceIt.Services
+{
+    public static class CacheManagerService
+    {
+        public static void ClearWebCache()
+        {
+            DependencyService.Get<ICacheManagerService>().ClearWebCache();
+        }
+    }
+}
---
+++ TraceIt/Services/ICacheManagerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceIt.Services
+{
+    public interface ICacheManagerService
+    {
+        void ClearWebCache();
+    }
+}
