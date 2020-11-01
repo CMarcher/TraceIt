8a6102ddacf8897f22102b790dabc6c860fd9cf4
CMarcher <calebandcorey@gmail.com>
2020-11-01 22:30:19 +1300

    New extensions to set a visual element's IsEnabled property to true or false for Android only.

---
+++ TraceIt/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TraceIt.Extensions
+{
+    public static class VisualElementExtensions
+    {
+        public static void SetEnabledForAndroid(this VisualElement element, bool enableElement)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+                element.IsEnabled = enableElement is true ? true : false;
+        }
+
+        public static void SetEnabledForAndroid(this ToolbarItem toolbarItem, bool enableElement)
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+                toolbarItem.IsEnabled = enableElement is true ? true : false;
+        }
+    }
+}
