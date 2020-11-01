d166d269882122334896108e71649a5ee9b8dcfa
CMarcher <calebandcorey@gmail.com>
2020-11-01 22:59:23 +1300

    Removed NavigationService. New StartAppCenter method added and called from OnStart.

--- TraceIt/App.xaml.cs
+++ TraceIt/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Threading.Tasks;
 using TraceIt.Models;
 using TraceIt.Services;
@@ -13,7 +16,6 @@ namespace TraceIt
 
     public partial class App : Application
     {
-        public static NavigationService NavigationService { get; private set; } = new NavigationService();
         public static DataService DataService { get; private set; } = new DataService();
         public static MessagingService MessagingService { get; private set; } = new MessagingService();
         public static DataRepository DataRepository { get; private set; } = new DataRepository();
@@ -27,6 +29,14 @@ namespace TraceIt
             LaunchPage();
         }
 
+        private void StartAppCenter()
+        {
+            string iOSKey = "ios=63e9764e-1878-44f1-9d77-8e672067b549;";
+            string androidKey = "android=e8acd4eb-f157-430e-a9c1-c7fb04dc78d4;";
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start(iOSKey + androidKey, typeof(Analytics), typeof(Crashes));
+        }
+
         private void LaunchPage()
         {
             if (UserManagerService.LoggedIn)
@@ -37,6 +47,7 @@ namespace TraceIt
 
         protected async override void OnStart()
         {
+            StartAppCenter();
             await Task.Run(DataRepository.InitialiseAsync);
         }
 
