dc6b2907d28a00d6302bcc3ce31865e54ca9ff28
CMarcher <calebandcorey@gmail.com>
2020-11-01 22:54:00 +1300

    New Intialise method, new Set Title method that sets Title based on StandardType. buttonAccess enabled and disabled for my new android nav pushing pattern.

--- TraceIt/Views/StandardDetailPage.xaml.cs
+++ TraceIt/Views/StandardDetailPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TraceIt.Extensions;
 using TraceIt.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,13 +18,31 @@ namespace TraceIt.Views
         public StandardDetailPage(Standard standard = null)
         {
             InitializeComponent();
-            BindingContext = standard;
-            Standard = standard;
+            Initialise(standard);
         }
 
-        private void buttonAccess_Clicked(object sender, EventArgs e)
+        private void Initialise(Standard standard = null)
         {
-            Navigation.PushAsync(new StandardWebPage(Standard));
+            BindingContext = standard;
+            Standard = standard;
+            SetTitle();
+        }
+
+        private void SetTitle()
+        {
+            string unitTitle = Standard.Code + " Details";
+            string achievementTitle = Standard.SubjectReference + " Details";
+
+            Title = Standard.StandardType is Standard.StandardTypes.Unit ? unitTitle : achievementTitle;
+        }
+
+        private async void buttonAccess_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            button.SetEnabledForAndroid(false);
+            await Navigation.PushAsync(new StandardWebPage(Standard));
+            button.SetEnabledForAndroid(true);
         }
     }
 }
