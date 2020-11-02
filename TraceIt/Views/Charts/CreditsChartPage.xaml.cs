0acedbc34fb23b86cf553cebb51173a55748ea65
CMarcher <calebandcorey@gmail.com>
2020-11-02 20:07:35 +1300

    Added CreditsTotalLabelConverter to resources and called from progress bar. Added SetProgressBarBinding method that uses the levelFilterSelector SelectedIndex to determine what property binding context should be set to.

--- TraceIt/Views/Charts/CreditsChartPage.xaml.cs
+++ TraceIt/Views/Charts/CreditsChartPage.xaml.cs
@@ -22,6 +22,27 @@ namespace TraceIt.Views.Charts
         {
             levelFilterSelector.SelectedIndex = 0;
             chartSelector.SelectedIndex = 0;
+            SetProgressBarBinding();
+        }
+
+        private void levelFilterSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetProgressBarBinding();
+        }
+
+        private void SetProgressBarBinding()
+        {
+            var index = levelFilterSelector.SelectedIndex;
+            var viewmodel = BindingContext as CreditsChartPageViewModel;
+
+            if (index is 0)
+                creditsProgressBar.SetBinding(BindingContextProperty, new Binding(nameof(viewmodel.LevelOneBreakdown)));
+            else if (index is 1)
+                creditsProgressBar.SetBinding(BindingContextProperty, new Binding(nameof(viewmodel.LevelTwoBreakdown)));
+            else if (index is 2)
+                creditsProgressBar.SetBinding(BindingContextProperty, new Binding(nameof(viewmodel.LevelThreeBreakdown)));
+            else
+                creditsProgressBar.SetBinding(BindingContextProperty, new Binding(nameof(viewmodel.OverallBreakdown)));
         }
     }
 }
