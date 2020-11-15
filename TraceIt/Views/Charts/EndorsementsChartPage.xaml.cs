f1e056dcb6eea53025429ee8e02d5f590c11743d
CMarcher <calebandcorey@gmail.com>
2020-11-15 18:25:36 +1300

    Removed GetEndorsementType method, as SubjectEndorsements are no longer filtered overall.

--- TraceIt/Views/Charts/EndorsementsChartPage.xaml.cs
+++ TraceIt/Views/Charts/EndorsementsChartPage.xaml.cs
@@ -45,42 +45,23 @@ namespace TraceIt.Views.Charts
             listViewEndorsements.DataSource.RefreshFilter();
             listViewEndorsements.DataSource.Refresh();
         }
-        
+
         private bool FilterEndorsements(object item)
         {
             if (item is SubjectEndorsement)
             {
                 var viewmodel = BindingContext as EndorsementsChartPageViewModel;
-                var endorsementType = GetEndorsementType();
                 var endorsement = item as SubjectEndorsement;
 
                 bool matchesSelectedYear = endorsement.Year == viewmodel.Year;
-                bool matchesEndorsementType = endorsement.EndorsementType == endorsementType;
 
-                return matchesSelectedYear && matchesEndorsementType;
+                return matchesSelectedYear;
             }
             else
                 throw new Exception("Invalid item type: " + item.GetType());
         }
 
-        private SubjectEndorsement.EndorsementTypes GetEndorsementType()
-        {
-            var index = endorsementTypeSelector.SelectedIndex;
-
-            if (index is 0)
-                return SubjectEndorsement.EndorsementTypes.Merit;
-            else if (index is 1)
-                return SubjectEndorsement.EndorsementTypes.Excellence;
-            else
-                return SubjectEndorsement.EndorsementTypes.Merit;
-        }
-
         private void SfSegmentedControl_SelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e)
-        {
-            RefreshDataSource();
-        }
-
-        private void endorsementTypeSelector_SelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e)
         {
             RefreshDataSource();
         }
