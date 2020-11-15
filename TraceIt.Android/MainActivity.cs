7f4e4b0b6d3f1ee6b2ac61a8094b81badee23b50
CMarcher <calebandcorey@gmail.com>
2020-11-15 18:32:07 +1300

    Icon setting removed. MainActivity no longer MainLauncher.

--- TraceIt.Android/MainActivity.cs
+++ TraceIt.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TraceIt.Droid
 {
-    [Activity(Label = "TraceIt", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TraceIt", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
