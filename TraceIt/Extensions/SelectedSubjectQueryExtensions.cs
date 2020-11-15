8854fdfb9338603eb802760c7f9c5ccfcb632f5f
CMarcher <calebandcorey@gmail.com>
2020-11-15 18:09:00 +1300

    Since SubjectEndorsement has been restructured, no longer need to add two for merit and excellence to subjectEndorsements under GetSubjectEndorsements. HasAchievedUniEntrance changed to account for whether the user has reached level 3, and whether each uni approved subject has achieved more than twelve credits. New GetLevelEndorsements method added.

--- TraceIt/Extensions/SelectedSubjectQueryExtensions.cs
+++ TraceIt/Extensions/SelectedSubjectQueryExtensions.cs
@@ -37,7 +37,7 @@ namespace TraceIt.Extensions
             var levelThreeBreakdown = new TotalBreakdown(selectedStandards, TotalBreakdown.Levels.Three);
             var allBreakdown = new TotalBreakdown(selectedStandards, TotalBreakdown.Levels.All);
 
-            var breakdowns = new ObservableCollection<TotalBreakdown>() 
+            var breakdowns = new ObservableCollection<TotalBreakdown>()
                     { levelOneBreakdown, levelTwoBreakdown, levelThreeBreakdown, allBreakdown };
 
             return breakdowns;
@@ -48,14 +48,8 @@ namespace TraceIt.Extensions
             subjects = subjects.GetSelectedSubjects();
             var subjectEndorsements = new ObservableCollection<SubjectEndorsement>();
 
-            Action<SelectedSubject> addEndorsements = subject =>
-            {
-                subjectEndorsements.Add(new SubjectEndorsement(subject, SubjectEndorsement.EndorsementTypes.Merit));
-                subjectEndorsements.Add(new SubjectEndorsement(subject, SubjectEndorsement.EndorsementTypes.Excellence));
-            };
-
-            foreach(var subject in subjects)
-                addEndorsements(subject);
+            foreach (var subject in subjects)
+                subjectEndorsements.Add(new SubjectEndorsement(subject));
 
             return subjectEndorsements;
         }
@@ -189,7 +183,7 @@ namespace TraceIt.Extensions
             var creditsPerSubject = new List<int>();
             int currentIndex = -1;
 
-            foreach(var standard in selectedStandards)
+            foreach (var standard in selectedStandards)
             {
                 if (matchesLastSubject(standard) is false)
                 {
@@ -198,17 +192,51 @@ namespace TraceIt.Extensions
                     currentIndex += 1;
                 }
 
-                if(uniEntranceFilter(standard))
+                if (uniEntranceFilter(standard))
                     creditsPerSubject[currentIndex] += standard.Credits;
             }
 
             creditsPerSubject = creditsPerSubject.OrderByDescending(x => x).Take(3).ToList();
-            var total = creditsPerSubject.Sum();
 
-            if (total < 36)
-                return false;
-            else
-                return true;
+            int firstTotal = 0;
+            int secondTotal = 0;
+            int thirdTotal = 0;
+
+            for (int index = 0; index < creditsPerSubject.Count; index++)
+            {
+                int credits = creditsPerSubject[index];
+                firstTotal += index is 0 ? credits : 0;
+                secondTotal += index is 1 ? credits : 0;
+                thirdTotal += index is 2 ? credits : 0;
+            }
+
+            var literacyAndNumeracy = subjects.GetLiteracyAndNumeracyCredits();
+            var readingAndWriting = subjects.GetReadingAndWritingCredits();
+            int literacyCredits = literacyAndNumeracy.Item1;
+            int numeracyCredits = literacyAndNumeracy.Item2;
+            int readingCredits = readingAndWriting.Item1;
+            int writingCredits = readingAndWriting.Item2;
+            bool hasLevelThree = subjects.HasAchievedLevelThree();
+            bool hasMetLiteracyAndNumeracyRequirements = literacyCredits >= 5 && numeracyCredits >= 5 &&
+                                                         readingCredits >= 5 && writingCredits >= 5;
+
+            bool isEligibleForUniEntrance = firstTotal >= 12 && secondTotal >= 12 && thirdTotal >= 12 &&
+                                            hasMetLiteracyAndNumeracyRequirements && hasLevelThree;
+
+            return isEligibleForUniEntrance;
+        }
+
+        public static bool HasAchievedLevelThree(this IEnumerable<SelectedSubject> subjects)
+        {
+            var selectedStandards = subjects.GetSelectedStandards();
+
+            int levelThreeCredits = selectedStandards.CountCredits(x => x.Level is Standard.Levels.Three && 
+                                                                   x.FinalGrade >= Standard.Grade.Achieved);
+            int levelTwoCredits = Math.Min(selectedStandards.CountCredits(x => x.Level is Standard.Levels.Two &&
+                                                                      x.FinalGrade >= Standard.Grade.Achieved), 20);
+
+            bool hasAchievedLevelThree = (levelThreeCredits + levelTwoCredits) >= 80;
+            return hasAchievedLevelThree;
         }
 
         public static Tuple<int, int> GetReadingAndWritingCredits(this IEnumerable<SelectedSubject> subjects)
@@ -218,6 +246,20 @@ namespace TraceIt.Extensions
             var writingCredits = selectedStandards.CountCredits(x => (int)x.FinalGrade >= 2 && x.IsWriting is true);
 
             return new Tuple<int, int>(readingCredits, writingCredits);
+        }
+
+        public static List<LevelEndorsement> GetLevelEndorsements(this IEnumerable<SelectedSubject> subjects)
+        {
+            var filteredStandards = subjects.GetSelectedStandards();
+            var levelOneStandards = filteredStandards.Where(x => x.Level is Standard.Levels.One);
+            var levelTwoStandards = filteredStandards.Where(x => x.Level is Standard.Levels.Two);
+            var levelThreeStandards = filteredStandards.Where(x => x.Level is Standard.Levels.Three);
+
+            var levelOne = new LevelEndorsement(levelOneStandards);
+            var levelTwo = new LevelEndorsement(levelTwoStandards);
+            var levelThree = new LevelEndorsement(levelThreeStandards);
+
+            return new List<LevelEndorsement>() { levelOne, levelTwo, levelThree };
         }
     }
 }
