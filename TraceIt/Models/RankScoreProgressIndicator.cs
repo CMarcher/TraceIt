4a756f7c93f297016ef021af60b3593c7e130329
CMarcher <calebandcorey@gmail.com>
2021-03-21 13:07:30 +1300

    Added degrees functionality to RankScoreProgressIndicator

--- TraceIt/Models/RankScoreProgressIndicator.cs
+++ TraceIt/Models/RankScoreProgressIndicator.cs
@@ -14,19 +14,14 @@ namespace TraceIt.Models
             set => SetProperty(ref _status, value, nameof(Status));
         }
 
-        private string _title;
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value, nameof(Title));
-        }
-
         private int _progress;
         public int Progress
         {
             get => _progress;
             set => SetProperty(ref _progress, value, nameof(Progress));
         }
+
+        public List<string> DegreeList { get; private set; }
 
         public void SetStatus(int progress, int previousProgress = 0)
         {
@@ -43,6 +38,92 @@ namespace TraceIt.Models
                 Status = StepStatus.NotStarted;
             else if (isLessThanProgressButMoreThanPrevious)
                 Status = StepStatus.InProgress;
+
+            SetDegreesCollection();
+        }
+
+        private void SetDegreesCollection()
+        {
+            DegreeList = new List<string>();
+
+            if (Progress is 150)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Arts (BA)",
+                    "Bachelor of Education (Teaching) (BEd(Tchg))",
+                    "Bachelor of Education (Teaching English to Speakers of Other Languages) (BEd (TESOL))",
+                    "Bachelor of Fine Arts (BF)",
+                    "Bachelor of Music (BMus)",
+                    "Bachelor of Social Work (BSW)",
+                    "Bachelor of Sport, Health and Physical Education (BSportHPE)"
+                });
+
+            else if (Progress is 165)
+                AddDegrees(new string[] { "Bachelor of Science (BSc) – excluding Biomedical Science and Food Science and Nutrition" });
+
+            else if (Progress is 180)
+                AddDegrees(new string[] 
+                {
+                    "Bachelor of Commerce (BCom)",
+                    "Bachelor of Design (BDes)",
+                    "Bachelor of Property (BProp)",
+                    "Bachelor of Urban Planning (Honours) (BUrbPlan(Hons))"
+                });
+
+            else if (Progress is 200)
+                AddDegrees(new string[] { "Bachelor of Science (BSc) – Food Science and Nutrition" });
+
+            else if (Progress is 210)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Global Studies (BGlobalSt)",
+                    "Bachelor of Global Studies conjoints",
+                    "Bachelor of Arts conjoints",
+                    "Bachelor of Commerce conjoints",
+                    "Bachelor of Design conjoints",
+                    "Bachelor of Fine Arts conjoints",
+                    "Bachelor of Music conjoints",
+                    "Bachelor of Property conjoints",
+                    "Bachelor of Science conjoints"
+                });
+
+            else if (Progress is 230)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Architectural Studies (BAS)",
+                    "Bachelor of Nursing (BNurs)",
+                    "Bachelor of Nursing conjoints"
+                });
+
+            else if (Progress is 250)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Health Sciences (BHSc)",
+                    "Bachelor of Health Sciences conjoints"
+                });
+
+            else if (Progress is 260)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Advanced Science (Honours) (BAdvSci(Hons))",
+                    "Bachelor of Engineering (Honours) (BE(Hons))"
+                });
+
+            else if (Progress is 275)
+                AddDegrees(new string[]
+                {
+                    "Bachelor of Advanced Science (Honours) conjoints",
+                    "Bachelor of Engineering (Honours) conjoints"
+                });
+
+            else if (Progress is 280)
+                AddDegrees(new string[] { "Bachelor of Science (BSc) – Biomedical Science" });
+        }
+
+        private void AddDegrees(string[] degrees)
+        {
+            foreach (string degree in degrees)
+                DegreeList.Add(degree);
         }
     }
 }
